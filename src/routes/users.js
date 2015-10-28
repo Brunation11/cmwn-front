6075d8a35250274dbf3b65914859fd91bdf60604
MaxPRafferty <MaxPRafferty@gmail.com>
2015-10-28 16:10:41 -0400

    cleaned up users (still uses districts endpoint)

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,27 +3,30 @@ import React from 'react';
 import HttpManager from 'components/http_manager'
 import {Table, Column}  from 'components/table';
 
-const data = [
-    {name: 'Wavis', age: 10},
-    {name: 'Sonya', age: 13},
-    {name: 'Elgesor', age: 9}
-];
-
-var urlData = HttpManager.GET({url:'http://cmwn/districts'});
-urlData.then(res => {
-    alert('Data Recieved ' + JSON.stringify(res));
-});
-
 var Users = React.createClass({
-    render: function () {return (
-        <div>
-            users
-            <Table rows={data}>
-                <Column dataKey="name" />
-                <Column dataKey="age" />
-            </Table>
-        </div>
-    );}
+    users: [{id: '1', description: 'yes'}],
+    componentWillMount: function () {
+        this.getUsers();
+    },
+    getUsers: function () {
+        var urlData = HttpManager.GET({url:'http://cmwn/districts'});
+        urlData.then(res => {
+            debugger;
+            this.users = res.response.data;
+            this.forceUpdate();
+        });
+    },
+    render: function () {
+        return (
+            <div>
+                users
+                <Table rows={this.users}>
+                    <Column dataKey="id" />
+                    <Column dataKey="description" />
+                </Table>
+            </div>
+        );
+    }
 });
 
 export default Users;
