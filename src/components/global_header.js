eb6ecb7e26517f83fc38ca58e5a7f05b960b1660
MaxPRafferty <MaxPRafferty@gmail.com>
2015-10-28 16:33:25 -0400

    added global header elements

--- src/components/global_header.js
+++ src/components/global_header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 import Authorization from 'components/authorization'
 
@@ -9,7 +10,7 @@ var GlobalHeader = React.createClass({
     render: function () {
         return (
             <div class="global-header">
-                <div class="logo">Change My World Now</div>
+                <div class="logo"><Link to="/" >Change My World Now</Link></div>
                 <div class="logout"><a href='#' onClick={this.logout}>{LOGOUT}</a></div>
                 <div class="current-user-info">{CURRENT_USER_IS + Authorization.currentUserName}</div>
             </div>
--- src/components/globals.js
+++ src/components/globals.js
@@ -12,7 +12,17 @@ const GLOBALS = {
         PROFILE_IMAGE: 'https://upload.wikimedia.org/wikipedia/commons/1/17/F%C3%ABdor_Ivanovi%C4%8D_%C5%A0aljapin_as_Farlaf_by_Alexandr_Golovin.jpg',
         NAME: 'Alexandr Golovin',
         ID: 1
-    }
+    },
+    TOP_NAV: [
+        {URL: '/', TEXT: 'Home'},
+        {URL: '/members', TEXT: 'Members'},
+        {URL: '/roles', TEXT: 'Roles'},
+        {URL: '/districts', TEXT: 'Districts'},
+        {URL: '/orgainizations', TEXT: 'Organizations'},
+        {URL: '/groups', TEXT: 'Groups'},
+        {URL: 'uploadcsv', TEXT: 'Upload CSV'},
+        {URL: '/playground', TEXT: 'Cloudinary Image'}
+    ]
 }
 
 export default GLOBALS;
