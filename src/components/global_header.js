a26c2a48c8dbd46c41e27776c902e6ba6d7f3c81
Max Rafferty <maxprafferty@gmail.com>
2015-11-02 12:49:44 -0500

    use real images

--- src/components/global_header.js
+++ src/components/global_header.js
@@ -4,6 +4,7 @@ import {Link} from 'react-router';
 import Authorization from 'components/authorization'
 
 import LOGO_URL from 'media/logo.png';
+import LOGOUT_URL from 'media/pt_logout_on.png';
 const LOGOUT = 'logout';
 const CURRENT_USER_IS = 'You are logged in as ';
 
@@ -11,8 +12,8 @@ var GlobalHeader = React.createClass({
     render: function () {
         return (
             <div className="global-header">
-                <div className="logo" alt="Change My World Now"><img src={LOGO_URL} /><Link to="/" >Change My World Now</Link></div>
-                <div className="logout"><a href='#' onClick={this.logout}>{LOGOUT}</a></div>
+                <div className="logo" ><Link to="/" ><img alt="Change My World Now" src={LOGO_URL} />Change My World Now</Link></div>
+                <div className="logout"><a href='#' onClick={this.logout}><img src={LOGOUT_URL} alt={LOGOUT} />{LOGOUT}</a></div>
                 <div className="current-user-info">{CURRENT_USER_IS + Authorization.currentUserName}</div>
             </div>
         );
