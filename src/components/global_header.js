ab6785e27b87f49fcb3c111961fd2fef65861eb3
Max Rafferty <maxprafferty@gmail.com>
2015-11-02 13:42:59 -0500

    added hamburger styles

--- src/components/global_header.js
+++ src/components/global_header.js
@@ -5,15 +5,21 @@ import Authorization from 'components/authorization'
 
 import LOGO_URL from 'media/logo.png';
 import LOGOUT_URL from 'media/pt_logout_on.png';
+import MENU_URL from 'media/MenuIcon4.png';
 const LOGOUT = 'logout';
 const CURRENT_USER_IS = 'You are logged in as ';
-
+const MENU = 'Menu'
 var GlobalHeader = React.createClass({
     render: function () {
         return (
             <div className="global-header">
                 <div className="logo" ><Link to="/" ><img alt="Change My World Now" src={LOGO_URL} />Change My World Now</Link></div>
-                <div className="logout"><a href='#' onClick={this.logout}><img src={LOGOUT_URL} alt={LOGOUT} />{LOGOUT}</a></div>
+                <div className="menu"><a href="#" onClick={this.toggleMenu}>
+                    <img src={MENU_URL} alt={MENU} />{MENU}
+                </a></div>
+                <div className="logout"><a href='#' onClick={this.logout}> 
+                    <img src={LOGOUT_URL} alt={LOGOUT} />{LOGOUT} 
+                </a></div>
                 <div className="current-user-info">{CURRENT_USER_IS + Authorization.currentUserName}</div>
             </div>
         );
