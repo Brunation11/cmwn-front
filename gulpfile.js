b80fa42620ed50777f88626f682699d2ee4ae3d7
Max Rafferty <maxprafferty@gmail.com>
2015-11-06 11:06:07 -0500

    updated build config for prod

--- gulpfile.js
+++ gulpfile.js
@@ -40,17 +40,7 @@ gulp.task("build", ["webpack:build"]);
 gulp.task("webpack:build", function(callback) {
     // modify some webpack config options
     var myConfig = Object.create(webpackProdConfig);
-    myConfig.plugins = myConfig.plugins.concat(
-        new webpack.DefinePlugin({
-            "process.env": {
-                // This has effect on the react lib size
-                "NODE_ENV": JSON.stringify("production")
-            }
-        }),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin()
-    );
-
+    
     // run webpack
     webpack(myConfig, function(err, stats) {
         if(err) throw new gutil.PluginError("webpack:build", err);
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,8 +3,12 @@ var webpack = require('webpack');
 
 module.exports = {
     devtool: 'source-map',
+    resolve: {
+        root: path.resolve('./src'),
+        extensions: ['', '.js']
+    },
     entry: [
-        './src/index'
+        'app'
     ],
     output: {
         path: path.join(__dirname, 'build'),
@@ -29,6 +33,21 @@ module.exports = {
             test: /\.js$/,
             loaders: ['babel'],
             include: path.join(__dirname, 'src')
+        },
+        {
+            test: /\.css$/,
+            loaders: ['style', 'css']
+        },
+        {
+            test: /\.scss$/,
+            loaders: ['style', 'css', 'sass']
+        },
+        {
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            loaders: [
+                'file?hash=sha512&digest=hex&name=[hash].[ext]',
+                'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+            ]
         }]
     }
 };
