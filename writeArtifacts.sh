91c2117a46378af8bdc464d7b8e897ea53752915
Max Rafferty <maxprafferty@gmail.com>
2015-11-11 11:13:51 -0500

    created (crude, temporary) artifact push script

---
+++ writeArtifacts.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#forgive us our hacks as we forgive those who hack against us
+
+rm -rf ../artifact
+CURRENT_VERSION=$(echo "(function () {return require('./package.json').version;})()" | node -p)
+CURRENT_HEAD="$(git rev-parse HEAD)"
+(cd .. && git clone git@github.com:ginasink/cmwn-front-artifact.git artifact)
+cp ./build/* ../artifact
+(cd ../artifact && git add . && git commit -m $CURRENT_HEAD && git tag -a $CURRENT_VERSION -m $CURRENT_VERSION && git push origin master && git push origin --tags)
+
