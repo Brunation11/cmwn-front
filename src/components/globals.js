3f2f1af47ce2da5dc8a029b4fcd0c5aa68691cf9
Max Rafferty <maxprafferty@gmail.com>
2015-11-16 18:31:51 -0500

    improved defaults

--- src/components/globals.js
+++ src/components/globals.js
@@ -7,8 +7,11 @@
  */
 import _ from 'lodash';
 
+import DefaultProfile from 'media/profile_tranparent.png';
+
 const GLOBALS = _.defaults(window.__cmwn, {
-    API_URL: 'http://cmwn/api/',
+    API_URL: 'http://api.changemyworldnow.com/',
+    DEFAULT_PROFILE: DefaultProfile,
     CURRENT_USER: {
         PROFILE_IMAGE: 'https://upload.wikimedia.org/wikipedia/commons/1/17/F%C3%ABdor_Ivanovi%C4%8D_%C5%A0aljapin_as_Farlaf_by_Alexandr_Golovin.jpg',
         NAME: 'Alexandr Golovin',
