6828fd147a628db12380507ce1b12f2be002ab0d
Max Rafferty <maxprafferty@gmail.com>
2015-11-19 21:12:39 -0500

    use real id as default

--- src/components/globals.js
+++ src/components/globals.js
@@ -15,7 +15,7 @@ const GLOBALS = _.defaults(window.__cmwn, {
     CURRENT_USER: {
         PROFILE_IMAGE: 'https://upload.wikimedia.org/wikipedia/commons/1/17/F%C3%ABdor_Ivanovi%C4%8D_%C5%A0aljapin_as_Farlaf_by_Alexandr_Golovin.jpg',
         NAME: 'Alexandr Golovin',
-        ID: 1
+        ID: 99
     },
     TOP_NAV: [
         {URL: '/', TEXT: 'Home'},
