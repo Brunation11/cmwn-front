e5c36163412822379b7d12f82dc5c48fa7a5c7fc
Max Rafferty <maxprafferty@gmail.com>
2015-11-20 17:11:36 -0500

    persist userdata in localstorage

--- src/components/authorization.js
+++ src/components/authorization.js
@@ -7,15 +7,16 @@ class _Authorization {
     constructor(options = {}){
         options.onChangeUser = options.onChangeUser || _.noop;
         this.currentUser = {};
-        this.currentUser.name = GLOBALS.CURRENT_USER.NAME;
-        this.currentUser.id = GLOBALS.CURRENT_USER.ID;
+        this.currentUser.name = window.localStorage.userName || GLOBALS.CURRENT_USER.NAME;
+        this.currentUser.id = window.localStorage.userId || GLOBALS.CURRENT_USER.ID;
         this._userLoaded = new Promise( (resolve, rej) => { //eslint-disable-line no-unused-vars
             /** @TODO MPR, 11/19/15: handle failure */
             var getUser = HttpManager.GET({url: `${GLOBALS.API_URL}/users/me`});
             getUser.then(res => {
-                /** @TODO MPR, 11/19/15: store and retrieve from localstorage */
                 this.currentUser.name = res.response.data.first_name + ' ' + res.response.data.last_name;
+                window.localStorage.setItem('userName', res.response.data.first_name + ' ' + res.response.data.last_name);
                 this.currentUser.id = res.response.data.id;
+                window.localStorage.setItem('userId', res.response.data.id);
                 resolve(res.response.data);
             });
         });
