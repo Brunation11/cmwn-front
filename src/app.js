6bdab3aafcbbc12e94d844660ffd34e5d1f93282
Max Rafferty <maxprafferty@gmail.com>
2015-12-01 12:47:29 -0500

    broke routes into public and private

--- src/app.js
+++ src/app.js
@@ -5,23 +5,11 @@ import { Router, Link } from 'react-router';
 
 import GlobalHeader from 'components/global_header';
 import History from 'components/history';
+import GLOBALS from 'components/globals';
+import PublicRoutes from 'components/public_routes';
+import PrivateRoutes from 'components/private_routes';
 
 import Errors from 'components/errors';
-import Login from 'routes/login';
-import Users from 'routes/users';
-import Districts from 'routes/districts';
-import DistrictView from 'routes/districts/view';
-import DistrictEdit from 'routes/districts/edit';
-import Organizations from 'routes/organizations';
-import OrganizationView from 'routes/organizations/view';
-import OrganizationEdit from 'routes/organizations/edit';
-import Groups from 'routes/groups';
-import GroupView from 'routes/groups/view';
-import GroupEdit from 'routes/groups/edit';
-import Friends from 'routes/friends';
-import SuggestedFriends from 'routes/friends/suggested';
-import Profile from 'routes/students/profile';
-import StudentEdit from 'routes/students/edit';
 import Layout from 'layouts/one_col';
 import Home from 'routes/home';
 
@@ -61,21 +49,34 @@ var App = React.createClass({
 
 var Landing = React.createClass({
     render: function () {
+        var redirect;
+        if (GLOBALS.MODE != null && (~GLOBALS.MODE.indexOf('dev') || ~GLOBALS.MODE.indexOf('development'))) {
+            return (
+                <Layout>
+                    <h1>Welcome</h1>
+                    <p>This is a temporary landing page for ease of navigation to site sections
+                    as they are constructed.</p>
+                    <p>
+                        <Link to="/districts">districts</Link>
+                    </p>
+                    <p>
+                        <Link to="/organizations">organizations</Link>
+                    </p>
+                    <p>
+                        <Link to="/groups">groups</Link>
+                    </p>
+                </Layout>
+            );
+        }
+        redirect = window.setTimeout(function () {
+            History.replaceState(null, '/profile');
+        }, 10000);
+        History.listenBeforeUnload(() => {
+            window.clearTimeout(redirect);
+        });
+        Errors.show404();
         return (
-            <Layout>
-                <h1>Welcome</h1>
-                <p>This is a temporary landing page for ease of navigation to site sections
-                as they are constructed.</p>
-                <p>
-                    <Link to="/districts">districts</Link>
-                </p>
-                <p>
-                    <Link to="/organizations">organizations</Link>
-                </p>
-                <p>
-                    <Link to="/groups">groups</Link>
-                </p>
-            </Layout>
+            <div></div>
         );
     }
 });
@@ -84,28 +85,9 @@ const routes = {
     path: '/',
     component: App,
     indexRoute: {component: Landing},
-    childRoutes: [
-        { path: 'login(/)', component: Login},
-        { path: 'users(/)', component: Users },
-        { path: 'profile(/)', component: Profile},
-        { path: 'student/edit(/)', component: StudentEdit },
-        { path: 'districts(/)', component: Districts},
-        { path: 'district/:id(/)', component: DistrictView},
-        { path: 'district/:id/view(/)', component: DistrictView},
-        { path: 'district/:id/edit(/)', component: DistrictEdit},
-        { path: 'organizations(/)', component: Organizations},
-        { path: 'organization/:id(/)', component: OrganizationView},
-        { path: 'organization/:id/view(/)', component: OrganizationView},
-        { path: 'organization/:id/edit(/)', component: OrganizationEdit},
-        { path: 'groups(/)', component: Groups},
-        { path: 'group/:id(/)', component: GroupView},
-        { path: 'group/:id/view(/)', component: GroupView},
-        { path: 'group/:id/edit(/)', component: GroupEdit},
-        { path: 'friends(/)', component: Friends},
-        { path: 'friends/suggested(/)', component: SuggestedFriends},
-
+    childRoutes: PublicRoutes.concat(PrivateRoutes).concat([
         { path: '*', component: Landing},
-    ]
+    ])
 };
 
 function run() {
---
+++ src/private_routes.js
@@ -0,0 +1,10 @@
+import Home from 'routes/home';
+import Login from 'routes/login';
+
+var routes = [
+    { path: 'home(/)', component: Home},
+    { path: 'login(/)', component: Login}
+];
+
+export default routes;
+
