6bb00a1f91d05ea2d27bcb1ff7037b807a88ea53
Max Rafferty <maxprafferty@gmail.com>
2015-12-07 17:56:34 -0500

    added uuid, setAttr util fns

--- src/components/util.js
+++ src/components/util.js
@@ -20,6 +20,17 @@ var Util = {
         retVal = retVal === target.data || retVal === target ? notFoundValue : retVal;
         retVal = _.isArray(retVal) ? retVal : [retVal];
         return retVal;
+    },
+    addAttr: function (item, key, value) {
+        item[key] = value;
+        return value;
+    },
+    /** from http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript */
+    uuid: function () {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);//eslint-disable-line eqeqeq
+            return v.toString(16);
+        });
     }
 };
 
