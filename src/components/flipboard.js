747b85019ee90c642614c1be364510d2eedefc85
Max Rafferty <maxprafferty@gmail.com>
2015-12-07 17:56:49 -0500

    added transform property

--- src/components/flipboard.js
+++ src/components/flipboard.js
@@ -6,20 +6,25 @@ import {Panel} from 'react-bootstrap';
 import 'components/flipboard.scss';
 
 var FlipBoard = React.createClass({
+    getInitialState: function () {
+        return {
+            data: _.map(this.props.data || [], this.props.transform)
+        };
+    },
     getDefaultProps: function () {
         return {
             header: 'Flipboard',
-            data: [],
+            transform: _.identity,
             renderFlip: () => <div className="flip"></div>
         };
     },
     render: function () {
-        if(!this.props.data.length) {
+        if(!this.state.data.length) {
             return null;
         }
         return (<div className="flipboard">
             <Panel header={this.props.header} className="standard">
-                {_.map(this.props.data, item => this.props.renderFlip(item))}
+                {_.map(this.state.data, item => this.props.renderFlip(item))}
             </Panel>
         </div>);
     }
