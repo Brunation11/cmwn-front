a72816e476a2cca21afe5e36df8c9c5336c2b302
Max Rafferty <maxprafferty@gmail.com>
2015-12-07 12:50:53 -0500

    made routes dynamic

--- src/sitemap.js
+++ src/sitemap.js
@@ -3,8 +3,31 @@ import {Link} from 'react-router';
 import {Panel} from 'react-bootstrap';
 
 import Layout from 'layouts/one_col';
+import Authorization from 'components/authorization';
+import GLOBALS from 'components/globals';
+import HttpManager from 'components/http_manager';
 
 var Page = React.createClass({
+    getInitialState: function () {
+        return {
+            orgUuid: '28f58d8a-9926-11e5-8a85-acbc32a6b1bb',
+            groupUuid: '2903aa3c-9926-11e5-97df-acbc32a6b1bb'
+        };
+    },
+    componentDidMount: function () {
+        var getOrgData = HttpManager.GET({url: GLOBALS.API_URL + 'organizations'});
+        getOrgData.then(res => {
+            this.setState({
+                orgUuid: res.response.data[0].uuid
+            });
+        });
+        var getGroupData = HttpManager.GET({url: GLOBALS.API_URL + 'groups'});
+        getGroupData.then(res => {
+            this.setState({
+                groupUuid: res.response.data[0].uuid
+            });
+        });
+    },
     render: function () {
         return (
            <Layout>
@@ -17,23 +40,23 @@ var Page = React.createClass({
                         <li><Link to={'/users'}>My Friends and Network</Link></li>
                         <li><Link to={'/organizations'}>My Schools</Link></li>
                         <li><Link to={'/groups'}>My Classes</Link></li>
-                        <li><Link to={'/organization/28f58d8a-9926-11e5-8a85-acbc32a6b1bb/profile'}>School Profile</Link></li>
-                        <li><Link to={'/group/2903aa3c-9926-11e5-97df-acbc32a6b1bb/profile'}>Class Profile</Link></li>
+                        <li><Link to={'/organization/' + this.state.orgUuid + '/profile'}>School Profile </Link>(note that direct links may break if the database is refreshed. This is expected. Use listing pages to navigate to a valid profile in this case.)</li>
+                        <li><Link to={'/group/' + this.state.groupUuid + '/profile'}>Class Profile</Link></li>
                         <li><Link to={'/profile/'}>Student Profile</Link></li>
-                        <li><Link to={'/student/2944706c-9926-11e5-a358-acbc32a6b1bb'}>Student Profile (friend)</Link></li>
+                        <li><Link to={'/student/' + Authorization.currentUser.uuid}>Student Profile (friend)</Link></li>
                         <li><Link to={'/profile/edit'}>Edit Student Profile</Link></li>
                    </ul>
                </Panel>
                <Panel header="Administrative Links" className="standard">
                    <ul>
-                        <li><Link to={'/group/2903aa3c-9926-11e5-97df-acbc32a6b1bb/view'}>Class Admin Info</Link></li>
-                        <li><Link to={'/group/2903aa3c-9926-11e5-97df-acbc32a6b1bb/edit'}>Edit Class</Link></li>
-                        <li><Link to={'/organization/28f58d8a-9926-11e5-8a85-acbc32a6b1bb/view'}>School Admin Info</Link></li>
-                        <li><Link to={'/organization/28f58d8a-9926-11e5-8a85-acbc32a6b1bb/edit'}>Edit School</Link></li>
-                        <li><Link to={'/teacher/2944706c-9926-11e5-a358-acbc32a6b1bb/profile'}>Teacher Profile</Link></li>
-                        <li><Link to={'/teacher/2944706c-9926-11e5-a358-acbc32a6b1bb/edit'}>Edit Teacher</Link></li>
-                        <li><Link to={'/parent/2944706c-9926-11e5-a358-acbc32a6b1bb/profile'}>Parent Profile</Link></li>
-                        <li><Link to={'/parent/2944706c-9926-11e5-a358-acbc32a6b1bb/edit'}>Edit Parent</Link></li>
+                        <li><Link to={'/group/' + this.state.groupUuid + '/view'}>Class Admin Info</Link></li>
+                        <li><Link to={'/group/' + this.state.groupUuid + '/edit'}>Edit Class</Link></li>
+                        <li><Link to={'/organization/' + this.state.orgUuid + '/view'}>School Admin Info</Link></li>
+                        <li><Link to={'/organization/' + this.state.orgUuid + '/edit'}>Edit School</Link></li>
+                        <li><Link to={'/teacher/' + Authorization.currentUser.uuid + '/profile'}>Teacher Profile</Link></li>
+                        <li><Link to={'/teacher/' + Authorization.currentUser.uuid + '/edit'}>Edit Teacher</Link></li>
+                        <li><Link to={'/parent' + Authorization.currentUser.uuid + '//profile'}>Parent Profile</Link></li>
+                        <li><Link to={'/parent/' + Authorization.currentUser.uuid + '/edit'}>Edit Parent</Link></li>
                    </ul>
                </Panel>
            </Layout>
