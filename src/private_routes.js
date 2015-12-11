e722304a6e41fd8ed788c6763b578e44a9108403
Max Rafferty <maxprafferty@gmail.com>
2015-12-11 13:35:33 -0500

    added plural redirects

--- src/private_routes.js
+++ src/private_routes.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 
 import Users from 'routes/users';
 import Districts from 'routes/districts';
@@ -23,7 +24,10 @@ import Sitemap from 'sitemap';
 
 var redirect = function (path) {
     return function (nextState, transition) {
-        debugger;
+        _.each(nextState.params, (v, k) => {
+            path = path.replace(`:${k}`, v);
+        });
+
         transition(null, path);
     };
 };
@@ -45,20 +49,31 @@ var routes = [
     { path: 'districts(/)', component: Districts},
     { path: 'district(/)', onEnter: redirect('/districts')},
     { path: 'district/:id(/)', component: DistrictView},
+    { path: 'districts/:id(/)', onEnter: redirect('/district/:id(/)')},
     { path: 'district/:id/view(/)', component: DistrictView},
+    { path: 'districts/:id/view(/)', onEnter: redirect('/district/:id/view(/)')},
     { path: 'district/:id/edit(/)', component: DistrictEdit},
+    { path: 'districts:id/edit(/)', onEnter: redirect('/district/id:edit(/)')},
     { path: 'organizations(/)', component: Organizations},
-    { path: 'organization/:id', component: OrganizationProfile},/** @TODO MPR, 12/7/15: Figure out why this route is neccissary*/
+    { path: 'organization(/)', onEnter: redirect('/organizations')},
     { path: 'organization/:id(/)', component: OrganizationProfile},
+    { path: 'organizations/:id(/)', onEnter: redirect('/organization/:id')},
     { path: 'organization/:id/view(/)', component: OrganizationView},
+    { path: 'organizations/:id/view(/)', onEnter: redirect('/organization/:id/view')},
     { path: 'organization/:id/edit(/)', component: OrganizationEdit},
+    { path: 'organizations/:id/edit(/)', onEnter: redirect('/organization/:id/edit')},
     { path: 'organization/:id/profile(/)', component: OrganizationProfile},
+    { path: 'organizations/:id/profile(/)', onEnter: redirect('/organization/:id/profile')},
     { path: 'groups(/)', component: Groups},
-    { path: 'group/:id', component: GroupProfile},
+    { path: 'group(/)', onEnter: redirect('/groups')},
     { path: 'group/:id(/)', component: GroupProfile},
+    { path: 'groups/:id(/)', onEnter: redirect('/group/:id')},
     { path: 'group/:id/view(/)', component: GroupView},
+    { path: 'groups/:id/view(/)', onEnter: redirect('/group/:id/view')},
     { path: 'group/:id/edit(/)', component: GroupEdit},
+    { path: 'groups/:id/edit(/)', onEnter: redirect('/group/:id/edit')},
     { path: 'group/:id/profile(/)', component: GroupProfile},
+    { path: 'groups/:id/profile(/)', onEnter: redirect('/group/:id/profile')},
     { path: 'friends(/)', component: Friends},
     { path: 'friends/suggested(/)', component: SuggestedFriends}
 ];
