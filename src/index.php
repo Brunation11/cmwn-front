6aa14e8a33290dbe34b596abb99584167c30a5be
Max Rafferty <maxprafferty@gmail.com>
2016-01-12 12:53:33 -0500

    added conditional classes for IE

--- src/index.php
+++ src/index.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
-<html>
+<!--[if lt IE 7]>      <html class="ie6"> <![endif]-->
+<!--[if IE 7]>         <html class="ie7"> <![endif]-->
+<!--[if IE 8]>         <html class="ie8"> <![endif]-->
+<!--[if gt IE 8]><!--> <html>         <!--<![endif]-->
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -32,7 +35,7 @@
 ?>
         <div id="pageerror"><a href="/login"> </a></div>
         <div id="cmwn-app"></div>
-        <!--[if lt IE 9]>
+        <!--[if lte IE 9]>
             <script>
             (function(){
             var ef = function(){};
