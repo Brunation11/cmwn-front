614629536df8f0921a6b3237136e2ef57f6dbcc6
Max Rafferty <maxprafferty@gmail.com>
2016-01-14 18:32:53 -0500

    removed default flips

--- src/components/trophycase.js
+++ src/components/trophycase.js
@@ -9,11 +9,9 @@ import GLOBALS from 'components/globals';
 import 'components/trophycase.scss';
 
 import DISABLED_FLIP from 'media/flip-disabled.png';
-import ENABLED_FLIP from 'media/flip.png';
 
 const EARNED = 'Your earned flips: ';
 const IN_PROGRESS = 'Your in progress flips: ';
-const OUT_OF = ' out of ';
 const HEADINGS = {
     FLIPBOARD: 'Flipboard'
 };
@@ -34,31 +32,16 @@ var Trophycase = React.createClass({
         );
     },
     renderCase: function (props) {
-        var complete, inProgress, flips;
-        if (props.data == null) {
-            //return null; //uncomment once you want to only use real data
-            props.data = [
-                {uuid: 'polar-bear', description: 'lorem ipsum', title: 'Flip Title 475', partial: DISABLED_FLIP, image: ENABLED_FLIP, progress: 100},
-                {uuid: 'animal-id', description: 'lorem ipsum', title: 'Flip Title 375', partial: DISABLED_FLIP, image: ENABLED_FLIP, progress: 100},
-                {uuid: 'sea-turtle', description: 'lorem ipsum', title: 'Flip Title 225', partial: DISABLED_FLIP, image: ENABLED_FLIP, progress: 100}
-            ];
-        }
-        /** @TODO MPR, 1/7/16: remove this once we have real flip progress */
-        flips = _.map(props.data, item => {
-            item.progress = Math.floor(Math.random() * 100);
-            return item;
-        });
-        complete = _.filter(flips, item => item.progress === 100);
-        inProgress = _.difference(flips, complete);
-        if (flips.length === 0) {
+        var complete, inProgress;
+        if (props.data.length === 0) {
             return null;
         }
-        /** @TODO MPR, 1/7/16: remove this once we have real flip progress */
-        complete = flips;
+        complete = _.filter(props.data, item => item.progress === 100);
+        inProgress = _.difference(props.data, complete);
         return (
             <Panel className="standard" header={HEADINGS.FLIPBOARD}>
                 <div className="earned">
-                    {EARNED}<strong>{complete.length}</strong>{OUT_OF}<strong>{63/** @TODO MPR, 12/29/15: use a real number*/}</strong>
+                    {EARNED}<strong>{complete.length}</strong>
                     <img className="spacer" src={DISABLED_FLIP} />
                     {this.renderComplete(complete)}
                 </div>
