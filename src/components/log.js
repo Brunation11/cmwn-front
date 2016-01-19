61606506156a2a3fb5c866d258c88dded59873c4
Max Rafferty <maxprafferty@gmail.com>
2016-01-19 12:44:23 -0500

    added dev logger actions

--- src/components/log.js
+++ src/components/log.js
@@ -1,10 +1,31 @@
+/* eslint-disable no-console, no-debugger*/
+import GLOBALS from 'components/globals';
 
 class Logger {
     constructor() { }
-    info() {}
-    log() {}
-    warn() {}
-    error() {}
+    info() {
+        if (GLOBALS.MODE.toLowerCase() === 'dev' || GLOBALS.MODE.toLowerCase() === 'development') {
+            console.info(...arguments);
+        }
+    }
+    log() {
+        if (GLOBALS.MODE.toLowerCase() === 'dev' || GLOBALS.MODE.toLowerCase() === 'development') {
+            console.log(...arguments);
+        }
+    }
+    warn() {
+        if (GLOBALS.MODE.toLowerCase() === 'dev' || GLOBALS.MODE.toLowerCase() === 'development') {
+            console.warn(...arguments);
+        }
+    }
+    error() {
+        if (GLOBALS.MODE.toLowerCase() === 'dev' || GLOBALS.MODE.toLowerCase() === 'development') {
+            console.error(...arguments);
+            if (GLOBALS.debugging === true || window.debugging === true || this.debugging === true) {
+                debugger;
+            }
+        }
+    }
 }
 
 var Log = new Logger();
