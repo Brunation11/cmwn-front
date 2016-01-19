5425383332d90daf6ad479871c8c9301224f4720
Max Rafferty <maxprafferty@gmail.com>
2016-01-19 18:08:46 -0500

    nice logging colors

--- src/components/log.js
+++ src/components/log.js
@@ -5,28 +5,28 @@ class Logger {
     constructor() { }
     info() {
         if (GLOBALS.MODE.toLowerCase() === 'dev' || GLOBALS.MODE.toLowerCase() === 'development') {
-            console.info(...arguments);
+            console.info('%c User Action: ', 'color: #d0e2ec;', ...arguments);
         }
         trackJs.console.info(...arguments); //eslint-disable-line no-undef
         Rollbar.info(...arguments); //eslint-disable-line no-undef
     }
     log() {
         if (GLOBALS.MODE.toLowerCase() === 'dev' || GLOBALS.MODE.toLowerCase() === 'development') {
-            console.log(...arguments);
+            console.log('%c System: ', 'color: #bfe2ca;', ...arguments);
         }
         trackJs.console.log(...arguments); //eslint-disable-line no-undef
         Rollbar.log(...arguments); //eslint-disable-line no-undef
     }
     warn() {
         if (GLOBALS.MODE.toLowerCase() === 'dev' || GLOBALS.MODE.toLowerCase() === 'development') {
-            console.warn(...arguments);
+            console.warn('%c Warning: ', 'color: #fed88f;', ...arguments);
         }
         trackJs.console.warn(...arguments); //eslint-disable-line no-undef
         Rollbar.warn(...arguments); //eslint-disable-line no-undef
     }
     error() {
         if (GLOBALS.MODE.toLowerCase() === 'dev' || GLOBALS.MODE.toLowerCase() === 'development') {
-            console.error(...arguments);
+            console.error('%c Error: ', 'color: #f4858e; font-weight: bold;', ...arguments);
             if (GLOBALS.debugging === true || window.debugging === true || this.debugging === true) {
                 debugger;
             }
