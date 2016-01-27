b990031cf0e8ab180b04d21430c5509486104d39
Max Rafferty <maxprafferty@gmail.com>
2016-01-27 12:11:43 -0500

    enable mod_rewrite in vagrant

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,7 @@ fi
 sudo cp /etc/apache2/mods-available/socache_shmcb.load /etc/apache2/mods-enabled/
 sudo cp /etc/apache2/mods-available/ssl.* /etc/apache2/mods-enabled/
 sudo cp mods-available/proxy_http.load mods-enabled/
+sudo cp mods-available/rewrite.load mods-enabled/
 sudo rm /etc/apache2/sites-enabled/000-default.conf
 sudo a2enmod headers
 sudo a2enmod proxy
