d60841b3232647640cfb9145601ab6d5b5039227
Max Rafferty <maxprafferty@gmail.com>
2016-02-01 15:28:07 -0600

    added object bind polyfill

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,5 +1,6 @@
 import 'components/polyfills/xhr';
 import {btoa} from 'base64';
+import bind from 'components/polyfills/object'; //eslint-disable-line no-unused-vars
 
 if (window.btoa == null) {
     window.btoa = btoa;
