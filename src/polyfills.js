2916575744c5397433fd891e2fcfe468f25ea50f
Max Rafferty <maxprafferty@gmail.com>
2016-02-03 12:45:16 -0600

    Revert "added object bind polyfill"
    This reverts commit 65e5fdd50a0c8a1bb2f7c15bb7ee23d5530a4bdc.

--- src/polyfills.js
+++ src/polyfills.js
@@ -1,6 +1,5 @@
 import 'components/polyfills/xhr';
 import {btoa} from 'base64';
-import bind from 'components/polyfills/object'; //eslint-disable-line no-unused-vars
 
 if (window.btoa == null) {
     window.btoa = btoa;
