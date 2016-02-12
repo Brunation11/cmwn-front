5b6a648bc0c636d03943593ca9f4773ad864eb03
Max Rafferty <maxprafferty@gmail.com>
2016-02-12 14:11:59 -0600

    added code fields, CORE-404

--- src/routes/organizations/edit.js
+++ src/routes/organizations/edit.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Button, Input, Panel} from 'react-bootstrap';
 import HttpManager from 'components/http_manager';
-import Layout from 'layouts/two_col';
 import GLOBALS from 'components/globals';
 import Validate from 'components/validators';
 import History from 'components/history';
+import Form from 'components/form';
+
+import Layout from 'layouts/two_col';
 
 const HEADINGS = {
     EDIT_TITLE: 'Info',
@@ -66,18 +68,57 @@ var Edit = React.createClass({
                  />
                  <Button onClick={this.submitData} > Save </Button>
               </Panel>
-              <Panel header={HEADINGS.UPLOAD} className="standard">
-                <iframe width="0" height="0" border="0" name="dummyframe" id="dummyframe"></iframe>
-                <form method="post" target="dummyframe" encType="multipart/form-data" action={`${GLOBALS.API_URL}admin/importexcel`} onsubmit={e => e.preventDefault()}>
-                    <input type="hidden" name="_token" value={HttpManager.token} />
-                    <input type="hidden" name="organizations" value={this.props.params.id} />
-                    <input type="hidden" name="organization_id" value={this.props.params.id} />
-                    <Input type="file" name="yourcsv" chars="40" label="Upload Spreadsheet"/>
-                    <Button type="submit" >{LABELS.SUBMIT}</Button>
-                </form>
-              </Panel>
            </Layout>
          );
+    }
+});
+
+var BulkUpload = React.createClass({
+    getInitialState: function () {
+        studentCode: '',
+        teacherCode: ''
+    },
+    render: function () {
+        return (
+          <Panel header={HEADINGS.UPLOAD} className="standard">
+            <iframe width="0" height="0" border="0" name="dummyframe" id="dummyframe"></iframe>
+            <Form ref="formRef" method="post" target="dummyframe" encType="multipart/form-data" action={`${GLOBALS.API_URL}admin/importexcel`}
+                onsubmit={e => {
+                    try {
+                        e.preventDefault()
+                    } catch (err) {
+                        return false;
+                    }
+                }}
+            >
+                <input type="hidden" name="_token" value={HttpManager.token} />
+                <input type="hidden" name="organizations" value={this.props.params.id} />
+                <input type="hidden" name="organization_id" value={this.props.params.id} />
+                <Input type="file" name="yourcsv" chars="40" label="Upload Spreadsheet"/>
+                <Input
+                    type="text"
+                    value={this.state.teacherCode}
+                    placeholder="Teacher Access Code"
+                    label="Teacher Access Code"
+                    validate="required"
+                    ref="teacherInput"
+                    name="teacher_code"
+                    onChange={e => this.setState({teacherCode: e.target.value})} //eslint-disable-line camelcase
+                />
+                <Input
+                    type="text"
+                    value={this.state.studentCode}
+                    placeholder="Student Access Code"
+                    label="Student Access Code"
+                    validate="required"
+                    ref="studentInput"
+                    name="student_code"
+                    onChange={e => this.setState({studentCode: e.target.value})} //eslint-disable-line camelcase
+                />
+                <Button type="submit" >{LABELS.SUBMIT}</Button>
+            </Form>
+          </Panel>
+        );
     }
 });
 
