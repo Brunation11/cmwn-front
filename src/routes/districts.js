5531e6b9b33d57c970536a64c8a181a088101e20
Max Rafferty <maxprafferty@gmail.com>
2016-04-05 17:16:07 -0400

    district nav cleanup

--- src/routes/districts.js
+++ src/routes/districts.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
+import {Button, Panel} from 'react-bootstrap';
 import { connect } from 'react-redux';
 
 import Layout from 'layouts/two_col';
@@ -11,44 +12,28 @@ const CREATE_TEXT = 'Create District';
 
 var Component = React.createClass({
     renderCreateDistrict: function () {
-        //if (!this.isSuperAdmin) {
-        //    return null;
-        //}
+        /** @TODO MPR, 3/8/16: Actually check superuser */
         return (
             <p><a href={'/districts/create'}>{CREATE_TEXT}</a></p>
         );
     },
     render: function () {
-        /** @TODO MPR, 3/8/16: Actually check superuser */
-        /*data => {
-                    //sure exploiting this side effect in a render method is filthy but its also easy
-                    var wasSuper = this.isSuperAdmin;
-                    this.isSuperAdmin = _.reduce(data, (a, i) => a && i.can_update, true);
-                    if (wasSuper !== this.isSuperAdmin) {
-                        window.setTimeout(() => this.forceUpdate(), 0);
-                    }
-                    return data;
-                }*/
         return (
             <Layout className="district-list">
-                <header>
-                    <h2>{TITLE}</h2>
-                    <div >
-                        {this.renderCreateDistrict()}
-                    </div>
-                </header>
-                <Table data={this.props.data}>
-                    <Column dataKey="title"
-                        renderCell={(data, row) => (
-                            <a onClick={() => History.push('/districts/' + row.org_id)}>{_.startCase(data)}</a>
-                        )}
-                    />
-                    <Column dataKey="description" />
-                    <Column dataKey="created_at" renderHeader="Created" />
-                    <Column dataKey="updated_at" renderHeader="Last Updated"
-                        renderCell={data => (data == null ? 'never' : data)}
-                    />
-                </Table>
+                <Panel header="My Districts" className="standard" >
+                    <Table data={this.props.data} className="admin">
+                        <Column dataKey="title"
+                            renderCell={(data, row) => (
+                                <a onClick={() => History.push('/districts/' + row.org_id)}>{_.startCase(data)}</a>
+                            )}
+                        />
+                        <Column dataKey="description" />
+                        <Column dataKey="created_at" renderHeader="Created" />
+                        <Column dataKey="updated_at" renderHeader="Last Updated"
+                            renderCell={data => (data == null ? 'never' : data)}
+                        />
+                    </Table>
+                </Panel>
             </Layout>
         );
     }
