74489c86a7afa59d577d9a0ce5526a27e96b5c2d
Chuck "MANCHUCK" Reeves <chuck@ginasink.com>
2016-05-24 12:41:25 -0400

    Updating install to set up doker Updated docker-compose to have a php server

--- bin/install.sh
+++ bin/install.sh
@@ -1,11 +1,44 @@
 #!/usr/bin/env bash
-echo "Installing global packages"
 
-npm install -g eslint eslint-plugin-react eslint-plugin-babel babel-eslint gulp
+cat $PWD/bin/splash.txt
 
-echo "Installing hooks"
+
+echo "[ui-installer] Installing hooks"
 bash $PWD/bin/install-git-hooks.sh
 
-echo "Installing node modules"
+echo "[ui-installer] Installing global packages"
+npm install -g eslint eslint-plugin-react eslint-plugin-babel babel-eslint gulp
 
+echo "[ui-installer] Installing node modules"
 npm install
+
+echo "[ui-installer] building production"
+gulp build --production
+
+echo "[api-installer] Building docker containers"
+eval $(docker-machine env)
+
+docker-compose build
+
+DOCKER_IP=$(docker-machine ip)
+
+cat <<EOF
+[api-installer] Completed!
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!                                                    !!
+!!  You're all set and ready to go.  In order to hit  !!
+!!  api-local.changemyworldnow.com, you need to add   !!
+!!  the following to your /etc/hosts file:            !!
+!!                                                    !!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+$DOCKER_IP    local.changemyworldnow.com
+
+You then run:
+docker-compose up -d
+and you will be able to access the site locally in the browser
+
+Happy Coding!
+
+EOF
