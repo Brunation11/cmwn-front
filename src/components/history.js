8ce62552c501af64acc0abed03ee6a0da642e135
les-bres <les.bres.2@gmail.com>
2016-06-06 12:38:13 -0400

    adding stuff from good commits

--- src/components/history.js
+++ src/components/history.js
@@ -1,13 +1,17 @@
+import _ from 'lodash';
 import { browserHistory} from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 
 import Store from 'components/store';
 
-var history;
+var history = {
+    listen: _.noop,
+    listenBefore: _.noop
+};
 try {
     history = syncHistoryWithStore(browserHistory, Store);
 } catch(err) {
-    console.log(err);
+    console.log(err); //eslint-disable-line no-console
 }
 
 export default history;
