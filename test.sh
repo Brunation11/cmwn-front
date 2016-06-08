77efe423be9a21655d6da93cd666aae1395c0437
les-bres <les.bres.2@gmail.com>
2016-06-08 14:12:39 -0400

    fiddles with istanbul

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 export NODE_PATH=$(pwd)/src
 export BABEL_ENV="production"
 export NODE_ENV="production"
-mocha src/app.test.js --compilers js:babel-core/register --require ignore-styles --require ./src/testdom.js
+istanbul cover _mocha ./src/app.test.js --compilers js:babel-core/register --require ignore-styles --require ./src/testdom.js
 
