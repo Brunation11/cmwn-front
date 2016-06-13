bc3af77b93a849026719633ca8def8734ed770e1
les-bres <les.bres.2@gmail.com>
2016-06-13 12:14:49 -0400

    setting up some basic unit tests

---
+++ src/components/trophycase.test.js
@@ -0,0 +1,5 @@
+import React from 'react'; //eslint-disable-line no-unused-vars
+import assert from 'assert';
+import TestUtils from 'react-addons-test-utils';
+import { Provider } from 'react-redux';
+import {expect} from 'chai';
---
+++ src/routes/users/profile.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import assert from 'assert';
+import TestUtils from 'react-addons-test-utils';
+import { Provider } from 'react-redux';
+import {expect} from 'chai';
+
+describe('Profile', function() {
+	
+})
