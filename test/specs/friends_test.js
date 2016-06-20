c59575f1cd38752d9abbc61477a30555bb02385d
Denis Balobin <db2941@columbia.edu>
2016-06-20 11:42:48 -0400

    fixed errors -- test is passing now

--- test/specs/friends_test.js
+++ test/specs/friends_test.js
@@ -1,18 +1,20 @@
 var login_test = require("./login_test");
 
 
-describe('tests the view page for a class', function () {
-    it('should check the elements on class view page', function () {
+describe('tests the friends page', function () {
+    it('should check that friends profile link takes to right profile', function () {
     	var friendLink;
     	login_test.login('calm-swan005', 'ginger12');
     	browser.waitForExist('#navMenu', 999999);
     	browser.url('/friends');
     	browser.waitForExist('.friend');
-    	var href = browser.getAttribute('friend-link');
+    	var href = browser.getAttribute('.friend-link', 'href');
     	if(href[0].length === 1)
     		friendLink = href;
-    	if(href[0] > 0)
+    	if(href[0].length > 1)
     		friendLink = href[0];
-    	browser.click('.friend');
+    	browser.click('.linkText');
     	browser.waitForExist('.user-metadata');
     	expect(browser.getUrl()).to.equal(friendLink);
+   	})
+});
