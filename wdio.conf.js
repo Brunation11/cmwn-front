58962a036b2259d056767cd6486ba51ff599e6c6
Max Rafferty <maxprafferty@gmail.com>
2016-06-21 14:33:29 -0400

    run tests in spec root

--- wdio.conf.js
+++ wdio.conf.js
@@ -10,7 +10,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/specs/run_tests/changepass_test.js'
+        './test/specs/*.js'
     ],
     // Patterns to exclude.
     exclude: [
