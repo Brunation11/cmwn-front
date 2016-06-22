f3c43ae27dee3a6630890334147b00264a67110a
Denis Balobin <db2941@columbia.edu>
2016-06-22 15:14:31 -0400

    changed timeout to 60000ms

--- test/specs/districts_test.js
+++ test/specs/districts_test.js
@@ -5,14 +5,14 @@ describe('tests the friends page', function () {
     it('should check that friends profile link takes to right profile', function () {
     	var friendLink;
     	login_test.login('teacher', 'business2');
-    	browser.waitForExist('#navMenu', 999999);
+    	browser.waitForExist('#navMenu', 60000);
     	browser.url('/districts');
-        browser.waitForExist('.district-link', 999999);
+        browser.waitForExist('.district-link', 60000);
         var districtLink = browser.getAttribute('.district-link', 'href');
         var splitLink = districtLink.split('/');
         var districtId = splitLink[splitLink.length-1]; //this will be the id of the district
         browser.click('.district-link');
-        browser.waitForExist('.panel-heading', 999999);
+        browser.waitForExist('.panel-heading', 60000);
         expect(browser.getUrl()).to.contain(districtId); //have to do contain because the urls slightly differ
    	})
 });
--- test/specs/friends_test.js
+++ test/specs/friends_test.js
@@ -5,7 +5,7 @@ describe('tests the friends page', function () {
     it('should check that friends profile link takes to right profile', function () {
     	var friendLink;
     	login_test.login('calm-swan005', 'ginger12');
-    	browser.waitForExist('#navMenu', 999999);
+    	browser.waitForExist('#navMenu', 60000);
     	browser.url('/friends');
     	browser.waitForExist('.friend');
     	var href = browser.getAttribute('.friend-link', 'href');
@@ -14,7 +14,7 @@ describe('tests the friends page', function () {
     	if(href[0].length > 1)
     		friendLink = href[0];
     	browser.click('.linkText');
-    	browser.waitForExist('.user-metadata', 999999);
+    	browser.waitForExist('.user-metadata', 60000);
     	expect(browser.getUrl()).to.equal(friendLink);
    	})
 });
