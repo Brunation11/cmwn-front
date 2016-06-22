93c53a92fe93846f8eaf33359cd5b275c4bdac83
Annalise Mariottini <annaliseirene@msn.com>
2016-06-22 11:03:58 -0400

    improved powershell script, removed debug mode

--- bin/docker-run-node.ps1
+++ bin/docker-run-node.ps1
@@ -12,7 +12,8 @@ else {
     $args = 'bash'
 }
 $path = '/' + $PSScriptRoot -Replace '\\', '/' -Replace '/bin', '' -Replace 'C:', 'c'
-& 'C:\Program Files\Docker Toolbox\docker-machine.exe' env front -shell=powershell | Invoke-Expression
+$dmPath = (Get-Command docker-machine).Path
+& $dmPath env front -shell=powershell | Invoke-Expression
 docker run -it -e DOCKER_HOST_IP='192.168.99.100' -v $path`:/var/www cmwnfront_node $args
-echo "If that didn't work, try running this instead:"
-echo "docker run -it -e DOCKER_HOST_IP='192.168.99.100' -v $path`:/var/www cmwnfront_node $args"
+echo "# If that didn't work, try running this instead:"
+echo "# docker run -it -e DOCKER_HOST_IP='192.168.99.100' -v $path`:/var/www cmwnfront_node $args"
