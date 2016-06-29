4798b32cba3284f20fcd97e71f00701ed8edd10c
Adam Walzer <adamwalzer@gmail.com>
2016-06-29 13:17:16 -0400

    skribble get media mock

--- src/components/game_events.js
+++ src/components/game_events.js
@@ -40,6 +40,219 @@ export default function (eventPrefix, gameId, _links, exitCallback) {
         },
         Init: function () {
         },
+        getMedia: function (e) {
+            if (e.gameData.path === 'skribble/menu') {
+                e.respond({
+                    'type': 'folder',
+                    'media_id': '8418112873',
+                    'name': 'Menu',
+                    'can_overlap': false,
+                    'asset_type': 'folder',
+                    'items': [
+                        {
+                            'type': 'folder',
+                            'media_id': '8418142465',
+                            'name': 'Items',
+                            'can_overlap': false,
+                            'asset_type': 'folder'
+                        },
+                        {
+                            'type': 'folder',
+                            'media_id': '8418116569',
+                            'name': 'Messages',
+                            'can_overlap': false,
+                            'asset_type': 'folder'
+                        }
+                    ]
+                });
+            } else if (e.gameData.path === 'skribble/menu/items') {
+                e.respond({
+                    'type': 'folder',
+                    'media_id': '8418142465',
+                    'name': 'Items',
+                    'can_overlap': false,
+                    'asset_type': 'folder',
+                    'items': [
+                        {
+                            'type': 'folder',
+                            'media_id': '8418142737',
+                            'name': 'Animals',
+                            'can_overlap': false,
+                            'asset_type': 'folder'
+                        },
+                        {
+                            'type': 'folder',
+                            'media_id': '8418131397',
+                            'name': 'Backgrounds',
+                            'can_overlap': false,
+                            'asset_type': 'folder'
+                        }
+                    ]
+                });
+            } else if (e.gameData.path === 'skribble/menu/items/animals') {
+                e.respond({
+                    'type': 'folder',
+                    'media_id': '8418142737',
+                    'name': 'Animals',
+                    'can_overlap': false,
+                    'asset_type': 'folder',
+                    'items': [
+                        {
+                            'type': 'folder',
+                            'media_id': '8418143185',
+                            'name': 'Dogs and Cats',
+                            'can_overlap': false,
+                            'asset_type': 'folder'
+                        }
+                    ]
+                });
+            } else if (e.gameData.path === 'skribble/menu/items/animals/dogs & cats') {
+                e.respond({
+                    'type': 'folder',
+                    'media_id': '8418143185',
+                    'name': 'Dogs and Cats',
+                    'can_overlap': false,
+                    'asset_type': 'folder',
+                    'items': [
+                        {
+                            'type': 'file',
+                            'media_id': '70116569037',
+                            'name': 'img_animals_sprite.png',
+                            'can_overlap': false,
+                            'asset_type': 'item',
+                            'check': {
+                                'type': 'sha1'
+                            },
+                            'src': 'https://media.changemyworldnow.com/f/70116569037'
+                        },
+                        {
+                            'type': 'file',
+                            'media_id': '70116576425',
+                            'name': 'img_dogs_1-01.png',
+                            'can_overlap': false,
+                            'asset_type': 'item',
+                            'check': {
+                                'type': 'sha1'
+                            },
+                            'src': 'https://media.changemyworldnow.com/f/70116576425'
+                        },
+                        {
+                            'type': 'file',
+                            'media_id': '70116583497',
+                            'name': 'img_dogs_10-01.png',
+                            'can_overlap': false,
+                            'asset_type': 'item',
+                            'check': {
+                                'type': 'sha1'
+                            },
+                            'src': 'https://media.changemyworldnow.com/f/70116583497'
+                        },
+                        {
+                            'type': 'file',
+                            'media_id': '70116593841',
+                            'name': 'img_dogs_2-01.png',
+                            'can_overlap': false,
+                            'asset_type': 'item',
+                            'check': {
+                                'type': 'sha1'
+                            },
+                            'src': 'https://media.changemyworldnow.com/f/70116593841'
+                        },
+                        {
+                            'type': 'file',
+                            'media_id': '70116600061',
+                            'name': 'img_dogs_3-01.png',
+                            'can_overlap': false,
+                            'asset_type': 'item',
+                            'check': {
+                                'type': 'sha1'
+                            },
+                            'src': 'https://media.changemyworldnow.com/f/70116600061'
+                        },
+                        {
+                            'type': 'file',
+                            'media_id': '70116607013',
+                            'name': 'img_dogs_4-01.png',
+                            'can_overlap': false,
+                            'asset_type': 'item',
+                            'check': {
+                                'type': 'sha1'
+                            },
+                            'src': 'https://media.changemyworldnow.com/f/70116607013'
+                        },
+                        {
+                            'type': 'file',
+                            'media_id': '70116615649',
+                            'name': 'img_dogs_5-01.png',
+                            'can_overlap': false,
+                            'asset_type': 'item',
+                            'check': {
+                                'type': 'sha1'
+                            },
+                            'src': 'https://media.changemyworldnow.com/f/70116615649'
+                        },
+                        {
+                            'type': 'file',
+                            'media_id': '70116620941',
+                            'name': 'img_dogs_6-01.png',
+                            'can_overlap': false,
+                            'asset_type': 'item',
+                            'check': {
+                                'type': 'sha1'
+                            },
+                            'src': 'https://media.changemyworldnow.com/f/70116620941'
+                        },
+                        {
+                            'type': 'file',
+                            'media_id': '70116631725',
+                            'name': 'img_dogs_7-01.png',
+                            'can_overlap': false,
+                            'asset_type': 'item',
+                            'check': {
+                                'type': 'sha1'
+                            },
+                            'src': 'https://media.changemyworldnow.com/f/70116631725'
+                        },
+                        {
+                            'type': 'file',
+                            'media_id': '70116637569',
+                            'name': 'img_dogs_8-01.png',
+                            'can_overlap': false,
+                            'asset_type': 'item',
+                            'check': {
+                                'type': 'sha1'
+                            },
+                            'src': 'https://media.changemyworldnow.com/f/70116637569'
+                        },
+                        {
+                            'type': 'file',
+                            'media_id': '70116649409',
+                            'name': 'img_dogs_9-01.png',
+                            'can_overlap': false,
+                            'asset_type': 'item',
+                            'check': {
+                                'type': 'sha1'
+                            },
+                            'src': 'https://media.changemyworldnow.com/f/70116649409'
+                        },
+                        {
+                            'type': 'file',
+                            'media_id': '70116658101',
+                            'name': 'img_dogs_sprite-01.png',
+                            'can_overlap': false,
+                            'asset_type': 'item',
+                            'check': {
+                                'type': 'sha1'
+                            },
+                            'src': 'https://media.changemyworldnow.com/f/70116658101'
+                        }
+                    ]
+                });
+            }
+        },
+        getFriends: function () {
+
+        }
     };
 
     var events = DEFAULT_EVENTS;
