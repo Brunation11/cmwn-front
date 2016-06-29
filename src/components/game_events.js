613207c637b0369fdbcd097754f685d7631307b9
Max Rafferty <maxprafferty@gmail.com>
2016-06-29 17:52:20 -0400

    added path resolver to game events

--- src/components/game_events.js
+++ src/components/game_events.js
@@ -4,10 +4,26 @@ import _ from 'lodash';
 import HttpManager from 'components/http_manager';
 import Toast from 'components/toast';
 import Log from 'components/log';
+import GLOBALS from 'components/globals';
 
 import Skribble from 'components/game_events/skribble';
 
 const BAD_FLIP = 'There was a problem registering your earned flip. Please try again in a little while';
+
+var resolveFinalMediaState = function (segments, result) {
+    var url = GLOBALS.API_URL + 'media';
+    var splitSegments = segments.split('/');
+    var nextSegment = segments[0];
+
+    if (splitSegments.length && result != null) return Promise.resolve(result);
+
+    if (result != null) {
+        url = _.find(result._embedded.items, i => i.name === nextSegment);
+        if (!url) return Promise.reject('Error: 404, provided media path does not exist');
+    }
+
+    return HttpManager.GET(url).then(server => resolveFinalMediaState(segments, server.response));
+};
 
 export default function (eventPrefix, gameId, _links, exitCallback) {
     var submitFlip = function (flip) {
@@ -19,6 +35,8 @@ export default function (eventPrefix, gameId, _links, exitCallback) {
             Log.log('Server refused flip update', err, flip);
         });
     };
+
+    var events;
 
     const DEFAULT_EVENTS = {
         flipped: function (e) {
@@ -46,214 +64,11 @@ export default function (eventPrefix, gameId, _links, exitCallback) {
         setData: function () {
         },
         getMedia: function (e) {
-            if (e.gameData.path === 'skribble/menu') {
-                e.respond({
-                    'type': 'folder',
-                    'media_id': '8418112873',
-                    'name': 'Menu',
-                    'can_overlap': false,
-                    'asset_type': 'folder',
-                    'items': [
-                        {
-                            'type': 'folder',
-                            'media_id': '8418142465',
-                            'name': 'Items',
-                            'can_overlap': false,
-                            'asset_type': 'folder'
-                        },
-                        {
-                            'type': 'folder',
-                            'media_id': '8418116569',
-                            'name': 'Messages',
-                            'can_overlap': false,
-                            'asset_type': 'folder'
-                        }
-                    ]
-                });
-            } else if (e.gameData.path === 'skribble/menu/items') {
-                e.respond({
-                    'type': 'folder',
-                    'media_id': '8418142465',
-                    'name': 'Items',
-                    'can_overlap': false,
-                    'asset_type': 'folder',
-                    'items': [
-                        {
-                            'type': 'folder',
-                            'media_id': '8418142737',
-                            'name': 'Animals',
-                            'can_overlap': false,
-                            'asset_type': 'folder'
-                        },
-                        {
-                            'type': 'folder',
-                            'media_id': '8418131397',
-                            'name': 'Backgrounds',
-                            'can_overlap': false,
-                            'asset_type': 'folder'
-                        }
-                    ]
-                });
-            } else if (e.gameData.path === 'skribble/menu/items/animals') {
-                e.respond({
-                    'type': 'folder',
-                    'media_id': '8418142737',
-                    'name': 'Animals',
-                    'can_overlap': false,
-                    'asset_type': 'folder',
-                    'items': [
-                        {
-                            'type': 'folder',
-                            'media_id': '8418143185',
-                            'name': 'Dogs and Cats',
-                            'can_overlap': false,
-                            'asset_type': 'folder'
-                        }
-                    ]
-                });
-            } else if (e.gameData.path === 'skribble/menu/items/animals/dogs & cats') {
-                e.respond({
-                    'type': 'folder',
-                    'media_id': '8418143185',
-                    'name': 'Dogs and Cats',
-                    'can_overlap': false,
-                    'asset_type': 'folder',
-                    'items': [
-                        {
-                            'type': 'file',
-                            'media_id': '70116569037',
-                            'name': 'img_animals_sprite.png',
-                            'can_overlap': false,
-                            'asset_type': 'item',
-                            'check': {
-                                'type': 'sha1'
-                            },
-                            'src': 'https://media.changemyworldnow.com/f/70116569037'
-                        },
-                        {
-                            'type': 'file',
-                            'media_id': '70116576425',
-                            'name': 'img_dogs_1-01.png',
-                            'can_overlap': false,
-                            'asset_type': 'item',
-                            'check': {
-                                'type': 'sha1'
-                            },
-                            'src': 'https://media.changemyworldnow.com/f/70116576425'
-                        },
-                        {
-                            'type': 'file',
-                            'media_id': '70116583497',
-                            'name': 'img_dogs_10-01.png',
-                            'can_overlap': false,
-                            'asset_type': 'item',
-                            'check': {
-                                'type': 'sha1'
-                            },
-                            'src': 'https://media.changemyworldnow.com/f/70116583497'
-                        },
-                        {
-                            'type': 'file',
-                            'media_id': '70116593841',
-                            'name': 'img_dogs_2-01.png',
-                            'can_overlap': false,
-                            'asset_type': 'item',
-                            'check': {
-                                'type': 'sha1'
-                            },
-                            'src': 'https://media.changemyworldnow.com/f/70116593841'
-                        },
-                        {
-                            'type': 'file',
-                            'media_id': '70116600061',
-                            'name': 'img_dogs_3-01.png',
-                            'can_overlap': false,
-                            'asset_type': 'item',
-                            'check': {
-                                'type': 'sha1'
-                            },
-                            'src': 'https://media.changemyworldnow.com/f/70116600061'
-                        },
-                        {
-                            'type': 'file',
-                            'media_id': '70116607013',
-                            'name': 'img_dogs_4-01.png',
-                            'can_overlap': false,
-                            'asset_type': 'item',
-                            'check': {
-                                'type': 'sha1'
-                            },
-                            'src': 'https://media.changemyworldnow.com/f/70116607013'
-                        },
-                        {
-                            'type': 'file',
-                            'media_id': '70116615649',
-                            'name': 'img_dogs_5-01.png',
-                            'can_overlap': false,
-                            'asset_type': 'item',
-                            'check': {
-                                'type': 'sha1'
-                            },
-                            'src': 'https://media.changemyworldnow.com/f/70116615649'
-                        },
-                        {
-                            'type': 'file',
-                            'media_id': '70116620941',
-                            'name': 'img_dogs_6-01.png',
-                            'can_overlap': false,
-                            'asset_type': 'item',
-                            'check': {
-                                'type': 'sha1'
-                            },
-                            'src': 'https://media.changemyworldnow.com/f/70116620941'
-                        },
-                        {
-                            'type': 'file',
-                            'media_id': '70116631725',
-                            'name': 'img_dogs_7-01.png',
-                            'can_overlap': false,
-                            'asset_type': 'item',
-                            'check': {
-                                'type': 'sha1'
-                            },
-                            'src': 'https://media.changemyworldnow.com/f/70116631725'
-                        },
-                        {
-                            'type': 'file',
-                            'media_id': '70116637569',
-                            'name': 'img_dogs_8-01.png',
-                            'can_overlap': false,
-                            'asset_type': 'item',
-                            'check': {
-                                'type': 'sha1'
-                            },
-                            'src': 'https://media.changemyworldnow.com/f/70116637569'
-                        },
-                        {
-                            'type': 'file',
-                            'media_id': '70116649409',
-                            'name': 'img_dogs_9-01.png',
-                            'can_overlap': false,
-                            'asset_type': 'item',
-                            'check': {
-                                'type': 'sha1'
-                            },
-                            'src': 'https://media.changemyworldnow.com/f/70116649409'
-                        },
-                        {
-                            'type': 'file',
-                            'media_id': '70116658101',
-                            'name': 'img_dogs_sprite-01.png',
-                            'can_overlap': false,
-                            'asset_type': 'item',
-                            'check': {
-                                'type': 'sha1'
-                            },
-                            'src': 'https://media.changemyworldnow.com/f/70116658101'
-                        }
-                    ]
-                });
-            }
+            e.gameData = e.gameData || {};
+            e.gameData.path = e.gameData.path || 'Skribble/Menu';
+            resolveFinalMediaState(e.gameData.path)
+                .then(response => e.respond(response._embedded.items))
+                .catch(err => Log.error(err));
         },
         getFriends: function (e) {
             /* eslint-disable */
@@ -1231,7 +1046,9 @@ export default function (eventPrefix, gameId, _links, exitCallback) {
         }
     };
 
-    var events = DEFAULT_EVENTS;
+    DEFAULT_EVENTS.getMedia({});
+
+    events = DEFAULT_EVENTS;
     if (gameId === 'skribble') {
         events = _.defaults(Skribble, events);
     }
