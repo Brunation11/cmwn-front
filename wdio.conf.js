0463482d9556ba0f5639f3253d40f784cb494115
Annalise Mariottini <annaliseirene@msn.com>
2016-06-29 11:27:30 -0400

    increased some waitFor times

--- wdio.conf.js
+++ wdio.conf.js
@@ -11,7 +11,8 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/specs/*.js'
+        './test/specs/school_profile_test.js'
+        //'./test/specs/*.js'
     ],
     // Patterns to exclude.
     exclude: [
@@ -68,7 +69,7 @@ exports.config = {
     baseUrl: 'https://local.changemyworldnow.com',
     //
     // Default timeout for all waitFor* commands.
-    waitforTimeout: 10000,
+    waitforTimeout: 30000,
     //
     // Default timeout in milliseconds for request
     // if Selenium Grid doesn't send response
