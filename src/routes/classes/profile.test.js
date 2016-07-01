e148b2431bf6dacbd6f28937f51c2906c555c077
Denis Balobin <db2941@columbia.edu>
2016-07-01 15:13:35 -0400

    fixed lint errors

--- src/routes/classes/profile.test.js
+++ src/routes/classes/profile.test.js
@@ -1,37 +1,35 @@
-import React from 'react';
+import React from 'react'; // eslint-disable-line no-unused-vars
 import { expect } from 'chai';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import { Profile } from 'routes/classes/profile';
 
 import teacherData from 'mocks/users/teacher_data';
-import studentDataA from 'mocks/users/student_data_a';
-import studentDataB from 'mocks/users/student_data_b';
 import classData from 'mocks/users/classData';
 
 
-var checkElementsTeacher = function(data, currentUser){
-	var profile = <Profile data={data} loading={false} currentUser={currentUser}/>;
-	const wrapper = shallow(profile);
-	expect(wrapper.instance()).to.be.instanceOf(Profile);
-	var layout = wrapper.find('Layout');
-	expect(layout).to.have.length(1);
-}
+var checkElementsTeacher = function (data, currentUser){
+    var profile = <Profile data={data} loading={false} currentUser={currentUser}/>;
+    var layout = wrapper.find('Layout');
+    var wrapper = shallow(profile);
+    expect(wrapper.instance()).to.be.instanceOf(Profile);
+    expect(layout).to.have.length(1);
+};
 
-var checkElementsNullData = function(currentUser){
-	var profile = <Profile data={null} loading={false} currentUser={currentUser}/>;
-    const wrapper = shallow(profile);
+var checkElementsNullData = function (currentUser){
+    var profile = <Profile data={null} loading={false} currentUser={currentUser}/>;
+    var wrapper = shallow(profile);
     expect(wrapper.instance()).to.be.instanceOf(Profile);
     expect(wrapper.children()).to.have.length(0);
-}
+};
 
-describe('<Profile />', function(){
-	describe('check elements', function(){
-		it('checks profile as teacher', function(){
-			checkElementsTeacher(classData, teacherData);
-		});
-		it('checks profile with null data', function(){
-			checkElementsNullData(teacherData);
-		});
-	});
+describe('<Profile />', function (){ // eslint-disable-line no-undef
+    describe('check elements', function (){ // eslint-disable-line no-undef
+        it('checks profile as teacher', function (){ // eslint-disable-line no-undef
+            checkElementsTeacher(classData, teacherData);
+        });
+        it('checks profile with null data', function (){ // eslint-disable-line no-undef
+            checkElementsNullData(teacherData);
+        });
+    });
 });
