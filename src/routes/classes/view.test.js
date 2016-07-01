15e34f4ed201575629156b9a6524f116c9b457f7
Denis Balobin <db2941@columbia.edu>
2016-07-01 15:03:30 -0400

    fixed lint errors

--- src/routes/classes/view.test.js
+++ src/routes/classes/view.test.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React from 'react'; // eslint-disable-line no-unused-vars
 import { expect } from 'chai';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import { View } from 'routes/classes/view';
 
 import teacherData from 'mocks/users/teacher_data';
-import studentDataA from 'mocks/users/student_data_a';
-import studentDataB from 'mocks/users/student_data_b';
 import classData from 'mocks/users/classData';
 
-var checkElements = function(wrapper) {
+var nullGroup = {group_id: null}; // eslint-disable-line camelcase
+
+var checkElements = function (data, currentUser) {
+    var view = <View data={data} currentUser = {currentUser}/>;
+    var wrapper = shallow(view);
+    expect(wrapper.instance()).to.be.instanceOf(View);
     expect(wrapper.find('Layout')).to.have.length(1);
     expect(wrapper.find('Panel')).to.have.length(2);
     expect(wrapper.find('p')).to.have.length(4);
@@ -17,22 +20,19 @@ var checkElements = function(wrapper) {
     expect(wrapper.find('#panel-2').children()).to.have.length(2);
 };
 
-var checkWithNullGroupId = function(wrapper){
-	expect(wrapper.children()).to.have.length(0);
-}
+var checkWithNullGroupId = function (data, currentUser){
+    var view = <View data={data} currentUser = {currentUser}/>;
+    var wrapper = shallow(view);
+    expect(wrapper.instance()).to.be.instanceOf(View);
+    expect(wrapper.children()).to.have.length(0);
+};
 
-describe('<View />', function () {
-	it('check that elements get rendered', () => {
-    	var view = <View data={classData} currentUser = {teacherData}/>;
-		var wrapper = shallow(view);
-    	expect(wrapper.instance()).to.be.instanceOf(View);
-    	checkElements(wrapper);
+describe('<View />', function () { // eslint-disable-line no-undef
+    it('check that elements get rendered', () => { // eslint-disable-line no-undef
+        checkElements(classData, teacherData);
+    }); // eslint-disable-line indent
+
+    it('check that render returns null if group_id is null', () => { // eslint-disable-line no-undef
+        checkWithNullGroupId(nullGroup, teacherData);
     });
-
-    it('check that render returns null if group_id is null', () => {
-    	var view = <View data={{group_id: null}} currentUser = {teacherData}/>;
-		var wrapper = shallow(view);
-		expect(wrapper.instance()).to.be.instanceOf(View);
-		checkWithNullGroupId(wrapper);
-    })
 });
