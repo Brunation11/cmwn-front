899fdf7a1176bdce203d6dabf791a3e996e9f52a
Denis Balobin <db2941@columbia.edu>
2016-07-07 11:48:20 -0400

    wrote unit test for edit

---
+++ src/routes/districts/edit.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+import { EditDistrict } from 'routes/districts/edit';
+
+import teacherData from 'mocks/users/teacher_data';
+import classData from 'mocks/users/class_data';
+
+var create = <EditDistrict data={teacherData._embedded.organizations} currentUser=
+	{teacherData} params={{id: '1001'}}/>;
+const WRAPPER = shallow(create);
+
+describe('check EditDistrict elements', function (){
+	it('checks that all elements are being rendered', function (){
+		expect(WRAPPER.find('Layout')).to.have.length(1);
+		expect(WRAPPER.find('Panel')).to.have.length(1);
+		expect(WRAPPER.find('Input')).to.have.length(2);
+		expect(WRAPPER.find('Button')).to.have.length(1);
+	});
+});
+
+describe('check CreateSchool elements', function (){
+	it('checks that all elements are being rendered', function (){
+		expect(WRAPPER.find('Form')).to.have.length(1);
+		expect(WRAPPER.find('Panel')).to.have.length(1);
+		expect(WRAPPER.find('Input')).to.have.length(2);
+		expect(WRAPPER.find('Button')).to.have.length(1);
+	});
+});
