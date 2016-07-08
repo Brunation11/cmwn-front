d775877481934d7a0b93a6ce9003c1864294b549
Winnie Lau <winniel@umich.edu>
2016-07-08 12:38:51 -0400

    Moved tests to specs folder

--- test/specs/run_tests/login.js
+++
@@ -1,9 +0,0 @@
-var exports = module.exports = {};
-exports.login = function (name, psswd) {
-   browser.url("/login");
-   browser.setValue('#email', name);
-   browser.setValue('#password', psswd);
-   browser.pause(2000); // doesn't work without the pause
-   browser.click('#login-button');
-   browser.waitForExist('.upload', 60000); // if login occurs "upload" button will be on page
-}
