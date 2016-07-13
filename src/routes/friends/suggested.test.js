2b066fef1499bd87b1f02200e1a1a66eb237b9b0
Denis Balobin <db2941@columbia.edu>
2016-07-13 10:41:06 -0400

    started working on the unit test

---
+++ src/routes/friends/suggested.test.js
@@ -0,0 +1,5 @@
+import React from 'react'; // eslint-disable-line no-unused-vars
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+import { Suggested } from 'routes/friends/suggested';
