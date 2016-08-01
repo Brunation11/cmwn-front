d1375b1f6bf367626f8c4f53aa1693e69cc3e8d6
Annalise Mariottini <annaliseirene@msn.com>
2016-08-01 12:13:10 -0400

    webpack production config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -69,7 +69,7 @@ module.exports = {
             test: /\.(jpe?g|png|gif|svg)$/i,
             loaders: [
                 'file?hash=sha512&digest=hex&name=[hash].[ext]',
-                'img?optimizationLevel=7'
+                'img?-minimize'
             ]
         }, {
             test: /\.woff$/,
