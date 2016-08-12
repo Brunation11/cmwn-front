945ce84fd6da26f5b53773a205a40a49b9bbdd03
bunation11 <bzatta@gmail.com>
2016-08-12 17:10:55 -0400

    add generator component for rendering generator page

---
+++ src/components/generator_link.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Button} from 'react-bootstrap';
+
+import Util from 'components/util';
+import History from 'components/history';
+
+const GENERATE = 'Generate User Cards';
+
+class GeneratorLink extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    componentDidMount() {
+        this.setupState(this.props);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setupState(nextProps);
+    }
+
+    setupState(props) {
+        var state = {};
+        state.base = props.base;
+        state.uuid = props.uuid || props.id;
+        // state.canUpdate = props.canUpdate !=
+        //     null ? props.canUpdate : Util.decodePermissions(props.scope).update;
+        this.setState(state);
+    }
+
+    render() {
+        // if ((!this.state.canUpdate && !this.state.scope) ||
+        //     (this.state.uuid == null && this.state.id == null)) {
+        //     return null;
+        // }
+        return (
+            <Button id="generate-btn" className={this.props.className + ' standard'}
+                onClick={() => History.push(`${this.state.base}/${this.state.uuid}/cards`)}>
+                {this.props.text ? this.props.text : GENERATE}</Button>
+        );
+    }
+}
+
+GeneratorLink.defaultProps = {base: ''};
+
+export default GeneratorLink;
