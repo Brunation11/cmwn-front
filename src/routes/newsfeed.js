c425f7d2cbc6ad89046a2ceb00360a39bc4b59f6
bunation11 <bzatta@gmail.com>
2016-09-09 12:13:12 -0400

    import necessary components and modules. refactor copy const. refactor default state in constructor. refactor component did mount and component will receive props functions. add functions for sorting feed data, and parsing game and notification feed items. add show modal, hide modal, and render game functions to launch games directly from the newsfeed. add render game feed item function to properly display feed items of type game. refactor render function to iterate through feed data and render appropriate feed style for each item type.

--- src/routes/newsfeed.js
+++ src/routes/newsfeed.js
@@ -1,76 +1,203 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Panel } from 'react-bootstrap';
+import { Panel, Modal } from 'react-bootstrap';
 import _ from 'lodash';
+import Shortid from 'shortid';
 
+import Detector from 'components/browser_detector';
 import GLOBALS from 'components/globals';
-import Layout from 'layouts/one_col';
+import Layout from 'layouts/two_col';
+import Game from 'components/game';
+
+import FlipBgDefault from 'media/flip-placeholder-white.png';
+import AvatarDefault from 'media/profile_transparent.png';
 
 import 'routes/newsfeed.scss';
 
 var mapStateToProps;
 var Page;
 
-const PAGE_UNIQUE_IDENTIFIER = 'feed';
+const COPY = {
+    PAGE_UNIQUE_IDENTIFIER: 'newsfeed',
+    HEADER: 'My News Feed',
+    BROWSER_NOT_SUPPORTED: (
+        <span>
+            <p>For the best viewing experience we recommend the desktop version in Chrome</p>
+            <p>If you don't have chrome,{' '}
+                <a href="https://www.google.com/chrome/browser/desktop/index.html" target="_blank">
+                    download it for free here
+                </a>.
+            </p>
+        </span>
+    )
+};
 
-const HEADER = 'My News Feed';
-
-export class Feed extends React.Component {
+export default class Feed extends React.Component {
     constructor() {
-        super ();
+        super();
 
-        this.state({
-            data: []
+        this.state = _.defaults({
+            data: [],
+            games: [],
+            notifications: []
         });
     }
 
     componentDidMount() {
-
+        var feed;
+        if (this.props.data && this.props.data._embedded && this.props.data._embedded.feed) {
+            feed = this.sortData(this.props.data._embedded.feed);
+            this.setState({
+                data: feed,
+                games: this.collectGames(feed) || [],
+                notifications: this.collectNotifications(feed) || []
+            });
+        }
     }
 
-    componentDidUpdate() {
+    componentWillReceiveProps(nextProps) {
+        var feed;
+        if (nextProps.data && nextProps.data._embedded && nextProps.data._embedded.feed) {
+            feed = this.sortData(nextProps.data._embedded.feed);
+            this.setState({
+                data: feed,
+                games: this.collectGames(feed) || [],
+                notifications: this.collectNotifications() || []
+            });
+        }
+    }
 
+    sortData(feed) {
+        return (
+            _.sortBy(feed, (item) => {
+                return new Date(item.created.data);
+            })
+        );
+    }
+
+    collectGames(feed) {
+        return (
+            _.filter(feed, ['type', 'game'])
+        );
+    }
+
+    collectNotifications(feed) {
+        return (
+            _.filter(feed, ['type', 'notification'])
+        );
+    }
+
+    showModal(gameUrl) {
+        var urlParts = gameUrl.split('/');
+        urlParts.pop(); // discard index.html
+        if (Detector.isMobileOrTablet() || Detector.isPortrait()) {
+            History.push(`/game/${_.last(urlParts)}`);
+        }
+        this.setState({
+            gameOn: true,
+            gameUrl,
+            game: _.last(urlParts),
+        });
+    }
+
+    hideModal() {
+        this.setState({gameOn: false});
+        this.refs.gameRef.dispatchPlatformEvent('quit');
+    }
+
+    renderGame() {
+        if (!window.navigator.standalone && (Detector.isMobileOrTablet() || Detector.isIe10())) {
+            return (
+                <div>
+                    {BROWSER_NOT_SUPPORTED}
+                    <p><a onClick={this.setState.bind(this, {gameOn: false})} >(close)</a></p>
+                </div>
+            );
+        }
+        return (
+            <div className="modal-game">
+                <Game
+                    ref="gameRef"
+                    isTeacher={!this.state.isStudent}
+                    url={this.state.gameUrl}
+                    onExit={this.setState.bind(this, {gameOn: false})}
+                    game={this.state.game}
+                    currentUser={this.props.currentUser}
+                />
+                <a id="close-modal" onClick={this.hideModal.bind(this)} className="modal-close">(close)</a>
+            </div>
+        );
+    }
+
+    renderGameFeedItem(item) {
+        var gameLink = item._links.games.href;
+        var gameID = _.last(gameLink.split('/'));
+        var avatar = `/flips/${gameID}-static.gif`;
+        var thumbnail = `${GLOBALS.GAME_URL}${gameID}/thumb.jpg`;
+        var onClick = this.showModal.bind(this, `${GLOBALS.GAME_URL}${gameID}/index.html`);
+
+        return (
+            <div className="feed-item" id={gameID} key={Shortid.generate()}>
+                <a onClick={onClick.bind(this)}>
+                    <div className="content">
+                        <div className="author">
+                            <div className="avatar">
+                                <img
+                                    src={avatar}
+                                    onError={() => {
+                                        this.onError = null;
+                                        this.src = AvatarDefault;
+                                    }}
+                                />
+                            </div>
+                            <span className="name">
+                                {item.sender || item.header}
+                            </span>
+                        </div>
+                        <div className="meta">
+                            {item.message}
+                        </div>
+                    </div>
+                    <img
+                        className="banner"
+                        src={thumbnail}
+                        onError={() => {
+                            this.onError = null;
+                            this.src = FlipBgDefault;
+                        }}
+                    />
+                </a>
+            </div>
+        );
     }
 
     render() {
+        if (!this.state.data) return null;
 
+        return (
+            <Layout className={COPY.PAGE_UNIQUE_IDENTIFIER} navMenuId="navMenu">
+                <Modal
+                    className="full-width game-modal"
+                    show={this.state.gameOn}
+                    onHide={this.hideModal.bind(this)}
+                    keyboard={false}
+                    backdrop="static"
+                    id="game-modal"
+                >
+                    <Modal.Body>
+                        {this.renderGame()}
+                    </Modal.Body>
+                </Modal>
+                <Panel className="standard" header={COPY.HEADER}>
+                    {_.map(this.state.data, (item) => {
+                        if (item.type === 'game') {
+                            return this.renderGameFeedItem(item);
+                        }
+                    })}
+                </Panel>
+            </Layout>
+        );
     }
-
-    // render: function () {
-    //     return (
-    //        <Layout className="newsfeed">
-    //             <Panel header={PAGETITLE} className="standard">
-    //                 <Fetcher url={GLOBALS.API_URL + 'games'} transform={data => {
-    //                     return _.map(data, item => {
-    //                         return {
-    //                             style: 'standard',
-    //                             image: {
-    //                                 url: GLOBALS.GAME_URL + item.uuid + '/thumb.jpg',
-    //                                 href: '/profile?open=' + item.uuid
-    //                             },
-    //                             message: [
-    //                                 {type: 'p', text: 'New Game!', attributes: {'className': 'new-game'}},
-    //                                 {type: 'h3', text: item.title},
-    //                                 {type: 'p', text: item.description},
-    //                                 {type: Button, text: 'Play Now', attributes: {'className':
-    //                                     'standard purple', onClick: () => {
-    //                                         window.location.href = '/profile?open=' + item.uuid;
-    //                                     }
-    //                                 }},
-    //                             ],
-    //                             source: 'ChangeMyWorldNow',
-    //                             sourceImage: cmwnIcon,
-    //                             created_at: item.created_at //eslint-disable-line
-    //                         };
-    //                     });
-    //                 }}>
-    //                     <Feed>
-    //                     </Feed>
-    //                 </Fetcher>
-    //             </Panel>
-    //        </Layout>
-    //     );
-    // }
 }
 
 mapStateToProps = state => {
@@ -90,5 +217,5 @@ mapStateToProps = state => {
 };
 
 Page = connect(mapStateToProps)(Feed);
-Page._IDENTIFIER = PAGE_UNIQUE_IDENTIFIER;
+Page._IDENTIFIER = COPY.PAGE_UNIQUE_IDENTIFIER;
 export default Page;
