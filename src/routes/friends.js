ecdec727dd1d70b863c42a7e7fc1ae7a8011f735
bunation11 <bzatta@gmail.com>
2016-10-07 16:20:57 -0400

    refactor friends route component. refactor consts and remove unused code. update add friend and accept request functions. extropolate and add functions for rendering request status, accept request and view profile buttons. refactor render user card and render card functions. refactor render function

--- src/routes/friends.js
+++ src/routes/friends.js
@@ -29,52 +29,121 @@ const FRIEND_PROBLEM = 'There was a problem adding your friend. Please try again
 const PROFILE = 'View Profile';
 const REQUESTED = 'Accept Request';
 const PENDING = 'Request Sent';
+const NO_FRIENDS = (
+    <h2 className="placeholder">
+        Looks like you haven't added any friends yet. Let's go{' '}
+        <Link to="/friends/suggested">find some!</Link>
+    </h2>
+);
 
 const PAGE_UNIQUE_IDENTIFIER = 'friends-page';
 
 var mapStateToProps;
 var Page;
 
-class Friends extends React.Component {
+export class Friends extends React.Component {
+    constructor(){
+        super();
+    }
 
     addFriend(item, e) {
-        var id = item.user_id != null ? item.user_id : item.friend_id;
-        var postBody = { 'friend_id': id };
         e.stopPropagation();
         e.preventDefault();
         ga('set', 'dimension7', 'send');
-        HttpManager.POST({url: this.props.currentUser._links.friend.href}, postBody).then(() => {
+
+        HttpManager.POST({
+            url: this.props.currentUser._links.friend.href
+        }, {
+            'friend_id': item.user_id != null ? item.user_id : item.friend_id
+        }).then(() => {
             this.refs.fetcher.getData().then(() => {
                 Toast.success(FRIEND_ADDED + item.username);
                 this.forceUpdate();
             });
             Actions.dispatch.START_RELOAD_PAGE(this.props);
-        }).catch(this.friendErr.bind(null, postBody));
+        }).catch((e) => {
+            Toast.error(FRIEND_PROBLEM);
+            Log.error(e, 'Friend request failed');
+        });
     }
 
     acceptRequest(item, e) {
-        var id = item.user_id != null ? item.user_id : item.friend_id;
-        var postBody = { 'friend_id': id };
         e.stopPropagation();
         e.preventDefault();
         ga('set', 'dimension7', 'recieved');
-        HttpManager.POST({url: this.props.currentUser._links.friend.href}, postBody).then(() => {
+
+        HttpManager.POST({
+            url: this.props.currentUser._links.friend.href
+        }, {
+            'friend_id': item.user_id != null ? item.user_id : item.friend_id
+        }).then(() => {
             Toast.success(FRIEND_ADDED + item.username);
             Actions.dispatch.START_RELOAD_PAGE(this.props);
-        }).catch(this.friendErr.bind(null, postBody));
+        }).catch((e) => {
+            Toast.error(FRIEND_PROBLEM);
+            Log.error(e, 'Friend request failed');
+        });
     }
 
-    doNothing(e) {
-        e.stopPropagation();
-        e.preventDefault();
+    renderRequestStatus(item) {
+        return (
+            <span
+                className={ClassNames(
+                    'request-status', {
+                        disabled: item.friend_status !== 'PENDING'
+                    }
+                )}
+            >
+                {PENDING}
+            </span>
+        );
     }
 
-    friendErr(e) {
-        Toast.error(FRIEND_PROBLEM);
-        Log.error(e, 'Friend request failed');
+    renderAcceptRequestButton(item) {
+        return (
+            <Button
+                onClick={this.acceptRequest.bind(this, item)}
+                className={ClassNames(
+                    'blue standard', {
+                        disabled: item.friend_status !== 'NEEDS_YOUR_ACCEPTANCE'
+                    }
+                )}
+            >
+                {REQUESTED}
+            </Button>
+        );
     }
 
-    renderFlip(item) {
+    renderViewProfileButton(item) {
+        return (
+            <a
+                className="btn purple standard"
+                href={`/profile/${item.user_id == null ? item.friend_id : item.user_id}`}
+            >
+                {PROFILE}
+            </a>
+        );
+    }
+
+    renderUserCard(item) {
+        return (
+            <div className="user-card" key={Shortid.generate()}>
+                <span className="overlay">
+                    <div className="prompts">
+                        {this.renderRequestStatus(item)}
+                        <br />
+                        {this.renderAcceptRequestButton(item)}
+                        <br />
+                        {this.renderViewProfileButton(item)}
+                    </div>
+                </span>
+                <img className="avatar" src={item.image}></img>
+                <p className="link-text" >{item.username}</p>
+            </div>
+        );
+    }
+
+    renderCard(item) {
         return (
             <Flag
                 data={item}
@@ -83,59 +152,13 @@ class Friends extends React.Component {
                     element={item}
                     trigger="click"
                 >
-                    {this.renderUserFlip.call(this, item)}
+                    {this.renderUserCard.call(this, item)}
                 </UserPopover>
             </Flag>
         );
     }
 
-    renderUserFlip(item) {
-        return (
-            <div className="flip" key={Shortid.generate()}>
-                <div className="item">
-                    <span className="overlay">
-                        <div className="relwrap">
-                            <div className="abswrap prompts">
-                                <span className={ClassNames(
-                                    'pending-prompt', {
-                                        disabled: item.friend_status !== 'PENDING'
-                                    }
-                                )}>
-                                    {PENDING}
-                                </span>
-                                <Button
-                                    onClick={this.acceptRequest.bind(this, item)}
-                                    className={ClassNames(
-                                        'blue standard', {
-                                            disabled: item.friend_status !== 'NEEDS_YOUR_ACCEPTANCE'
-                                        }
-                                    )}
-                                >
-                                    {REQUESTED}
-                                </Button>
-                                <a
-                                    href={`/profile/${item.user_id == null ? item.friend_id : item.user_id}`}
-                                    className="btn purple standard"
-                                >
-                                    {PROFILE}
-                                </a>
-                            </div>
-                        </div>
-                    </span>
-                    <img src={item.image}></img>
-                </div>
-                <p className="link-text" >{item.username}</p>
-            </div>
-        );
-    }
-
     render() {
-        const NO_FRIENDS = (
-                <h2 className="placeholder">
-                    Looks like you haven't added any friends yet. Let's go{' '}
-                    <Link to="/friends/suggested">find some!</Link>
-                </h2>
-        );
         if (this.props.data.length === 0) {
             return (
                 <Layout className={PAGE_UNIQUE_IDENTIFIER}>
@@ -143,38 +166,36 @@ class Friends extends React.Component {
                 </Layout>
             );
         }
-        const FRIENDS = (
-            <form>
-                <Paginator rowCount={this.props.rowCount} currentPage={this.props.currentPage}
-                    pageCount={this.props.pageCount} data={this.props.data} pagePaginator={true}>
-                    <FlipBoard
-                       // add conditional to check if user has flips
-                       // render either renderflip or renderuserflip
-                       renderFlip={this.renderFlip.bind(this)}
-                       header={HEADINGS.FRIENDS}
-                       transform={data => {
-                           var image;
-                           if (!_.has(data, '_embedded.image')) {
-                               image = GLOBALS.DEFAULT_PROFILE;
-                           } else {
-                               if (data._embedded.image.url != null) {
-                                   image = data._embedded.image.url;
-                               } else {
-                                   image = data.images.data[0].url;
-                               }
-                           }
 
-                           data = data.set('image', image);
-
-                           return data;
-                       }}
-                    />
-               </Paginator>
-            </form>
-        );
         return (
             <Layout className={PAGE_UNIQUE_IDENTIFIER}>
-                {FRIENDS}
+                <form>
+                    <Paginator
+                        rowCount={this.props.rowCount}
+                        currentPage={this.props.currentPage}
+                        pageCount={this.props.pageCount}
+                        data={this.props.data}
+                        pagePaginator={true}
+                    >
+                        <FlipBoard
+                           renderFlip={this.renderCard.bind(this)}
+                           header={HEADINGS.FRIENDS}
+                           transform={data => {
+                               var image;
+                               if (!_.has(data, '_embedded.image')) {
+                                   image = GLOBALS.DEFAULT_PROFILE;
+                               } else {
+                                   if (data._embedded.image.url != null) {
+                                       image = data._embedded.image.url;
+                                   } else {
+                                       image = data.images.data[0].url;
+                                   }
+                               }
+                               return data.set('image', image);
+                           }}
+                        />
+                   </Paginator>
+                </form>
             </Layout>
         );
     }
@@ -211,4 +232,3 @@ mapStateToProps = state => {
 Page = connect(mapStateToProps)(Friends);
 Page._IDENTIFIER = PAGE_UNIQUE_IDENTIFIER;
 export default Page;
-
