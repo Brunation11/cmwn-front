870b87081128ce581692c551c265078ee7beff15
bunation11 <bzatta@gmail.com>
2016-10-24 16:56:54 -0400

    refactor and remove unused constants. convert update username class to extension of react component. remove default state and add component did mount and component did receive props functions to update state.update api call routes with user links. refactor helper functions to update state with selected option. add functions for rendering generator, confirmation page, and re login notice page. update render function to display correct page.

--- src/components/update_username.js
+++ src/components/update_username.js
@@ -1,56 +1,63 @@
 import React from 'react';
-import {Panel, Button, Glyphicon} from 'react-bootstrap';
-import SweetAlert from 'sweetalert2';
+import {Button} from 'react-bootstrap';
 import ClassNames from 'classnames';
+import _ from 'lodash';
 
 import GLOBALS from 'components/globals';
 import HttpManager from 'components/http_manager';
-import Util from 'components/util';
-import Store from 'components/store';
+import Log from 'components/log';
+import Toast from 'components/toast';
 
 import 'components/update_username.scss';
 
-const IDENTIFIER = 'change-username';
-const CHANGE = 'create a new username';
-const CONFIRM_SET = 'Are you sure you want to change your username? You will not be able to change it back to {0}.'; //eslint-disable-line max-len
-const GOOD_UPDATED = 'Username updated to ';
-const BAD_UPDATE = 'Could not update your user name.';
+const GOOD_UPDATE = 'Username updated to ';
+const BAD_UPDATE = 'Could not update your user name to ';
 
-const BUTTONS = {
-    GENERATE: 'Generate New Name',
-    NEW: 'How about this one?',
-    PREVIOUS: 'This name is still available!',
-    ORIGINAL: 'You can also keep your original.',
-    CONFIRM: 'Yes, change it!',
-    CANCEL: 'No',
+const CALL_TO_ACTION = '*CLICK YOUR PREFERRED USERNAME';
+
+const LABELS = {
+    CURRENT: 'CURRENT USERNAME:',
+    SAVE: 'STEP 3: DON\'T FORGET TO'
 };
 
-const TITLES = {
-    SAVED: 'SAVED!',
-    ERROR: 'OH NO!'
+const BUTTONS = {
+    ORIGINAL: 'KEEP IT!'
 };
 
 const COPY = {
-    NOTE: 'Click to select the name you want.',
-    DISCLAIMER: 'Note: Once you change your username, you won\'t be able to go back to your original.'
+    CONFIRM_NOTICE_1: 'You will not be able to',
+    CONFIRM_NOTICE_2: 'get your old name again!',
+    LOGIN_NOTICE: 'Next time you will log in as:'
 };
 
-var UpdateUsername = React.createClass({
-    getInitialState: function () {
-        return {
-            username: this.props.username.slice(0, -3),
-            option: this.props.username.slice(0, -3),
-            last: this.props.username.slice(0, -3),
-            original: this.props.username.slice(0, -3),
-            loading: false
-        };
-    },
-    getDefaultProps: function () {
-        return {
-            copy: ''
-        };
-    },
-    reloadChildUsername: function () {
+export class UpdateUsername extends React.Component {
+    constructor() {
+        super();
+
+        this.state = _.defaults({
+            loading: false,
+            page: 'generator'
+        });
+    }
+
+    componentDidMount() {
+        if (this.props && this.props.currentUser) {
+            this.setState({
+                original: this.props.currentUser.username.slice(0, -3),
+                username: this.props.currentUser.username
+            });
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.currentUser !== nextProps.currentUser) {
+            this.setState({
+                original: nextProps.currentUser.username.slice(0, -3)
+            });
+        }
+    }
+
+    reloadChildUsername() {
         var self = this;
 
         var resetLoading = function () {
@@ -61,10 +68,16 @@ var UpdateUsername = React.createClass({
             }, 500);
         };
 
-        self.setState({loading: true});
+        self.setState({
+            loading: true
+        });
 
-        HttpManager.GET({url: `${GLOBALS.API_URL}user-name`}).then(server => {
+        HttpManager.GET({
+            url: this.props.currentUser._links.user_name.href
+        })
+        .then(server => {
             self.setState({
+                selected: null,
                 last: self.state.option,
                 option: server.response.user_name
             });
@@ -72,131 +85,180 @@ var UpdateUsername = React.createClass({
         }).catch(err => {  // eslint-disable-line
             resetLoading();
         });
+    }
 
-        self.setStyleOnClick();
-    },
-    setStyleOnClick: function () {
-        this.setState({tooltipsOpen: true});
-    },
-    setChildUsername: function () {
-        SweetAlert({
-            text: Util.formatString(CONFIRM_SET, this.state.original),
-            type: 'warning',
-            showConfirmButton: true,
-            confirmButtonText: BUTTONS.CONFIRM,
-            confirmButtonColor: '#47B72C',
-            confirmButtonClass: 'cmwn-font float-shadow',
-            showCancelButton: true,
-            cancelButtonText: BUTTONS.CANCEL,
-            cancelButtonColor: '#7829BB',
-            cancelButtonClass: 'cmwn-font float-shadow',
-            closeOnConfirm: false,
-            buttonStyling: false
-        }).then(isConfirm => {
-            if (isConfirm) {
-                HttpManager.POST({
-                    url: Store.getState().page.data._links.user_name.href
-                }, {
-                    user_name: this.state.option // eslint-disable-line
-                }).then(server => {
-                    this.setState({
-                        username: this.state.option
-                    });
-                    SweetAlert({
-                        title: TITLES.SAVED,
-                        text: (GOOD_UPDATED + server.response.username + '!'),
-                        type: 'success',
-                        timer: 3000,
-                        showConfirmButton: false
-                    });
-                }).catch(err => {
-                    SweetAlert({
-                        title: TITLES.ERROR,
-                        text: (BAD_UPDATE + (err.message ? ' Message: ' + err.message : '')),
-                        type: 'error',
-                        timer: 3000,
-                        showConfirmButton: false
-                    });
-                });
-            }
+    setChildUsername() {
+        HttpManager.POST({
+            url: this.props.currentUser._links.user_name.href
+        }, {
+            'user_name': this.state.option
+        }).then(server => {
+            this.setState({
+                username: server.response.username
+            });
+            Toast.success(`${GOOD_UPDATE}${server.response.username}`);
+        }).catch(err => {
+            Toast.error(`${BAD_UPDATE}${this.state.option}`);
+            Log.error(`Username not updated: ${err.message}`, err);
         });
-    },
-    resetLast: function () {
-        this.setState({option: this.state.last, last: this.state.option});
-    },
-    resetOriginal: function () {
-        this.setState({option: this.state.original, last: this.state.option});
-    },
-    updateAdultUsername: function () {
-        HttpManager.GET().then(server => {}).catch(err => {}); // eslint-disable-line
-    },
-    renderAdult: function () {},
-    renderChild: function () {
+
+        this.setState({
+            page: 'login-notice'
+        });
+    }
+
+    handleSelect(username) {
+        this.setState({
+            selected: username
+        });
+    }
+
+    setOriginal() {
+        this.setState({
+            selected: this.state.original,
+            page: 'login-notice'
+        });
+    }
+
+    setNewUsername() {
+        this.setState({
+            page: 'confirm'
+        });
+    }
+
+    renderGenerator() {
+        var self = this;
         return (
-           <div className="update-username-container">
-                <div className={ClassNames('animated loading', {
-                    hidden: !this.state.loading
-                })}>
+            <div className={`update-username-container ${this.state.page}`}>
+                {/* loader */}
+                <div className={ClassNames('animated loading', {hidden: !self.state.loading})}>
                     <div className="loader" />
                 </div>
-                <div className="left">
+
+                {/* Step 1 container */}
+                <div className="container step-1-container">
+                    {/* Step 1 icon */}
+                    <span className="step-1">1</span>
+                    {/* Generated username field */}
+                    <span className="generated">{self.state.option}</span>
+                    {/* Generate username button */}
                     <Button
-                        className="purple username-btn username-picker generate"
-                        onClick={this.reloadChildUsername}
+                        className="generate-btn animated slideInDown"
+                        onClick={self.reloadChildUsername.bind(self)}
+                    />
+                </div>
+
+                {/* Current username container */}
+                <div className="container current-username-container">
+                    {/* Current username field */}
+                    <span className="original-label">{LABELS.CURRENT}</span>
+                    <span className="original">{self.state.original}</span>
+                    {/* Keep current username button */}
+                    <Button
+                        className="original-btn"
+                        onClick={self.setOriginal.bind(self)} //update function to render login notice
                     >
-                        <Glyphicon glyph="repeat" /> {BUTTONS.GENERATE}
-                    </Button>
-                    <br />
-                    <Button className="green username-btn username-picker" id="cur-choice-btn"
-                        onClick={this.setChildUsername}>
-                        {BUTTONS.CONFIRM}
-                        <br />
-                        <span className="username username-picker">
-                            {this.state.option}
-                        </span>
-                    </Button>
-                    <br />
-                    <Button className="green username-btn username-picker" id="last-choice-btn"
-                            onClick={this.resetLast}>
-                        {BUTTONS.PREVIOUS}
-                        <br />
-                        <span className="username username-picker">
-                            {this.state.last}
-                        </span>
-                    </Button>
-                    <br />
-                    <Button className="green username-btn username-picker" id="original-choice-btn"
-                        onClick={this.resetOriginal}>
                         {BUTTONS.ORIGINAL}
-                        <br />
-                        <span className="username username-picker">
-                            {this.state.original}
-                        </span>
                     </Button>
                 </div>
-                <div className="right">
-                    <div className={ClassNames('note', {open: this.state.tooltipsOpen})}>
-                        <p>{COPY.NOTE}</p>
-                        <p className="disclaimer">{COPY.DISCLAIMER}</p>
-                    </div>
+
+                {/* Call to action container */}
+                <div className="container call-to-action-container">
+                    <span className="call-to-action">{CALL_TO_ACTION}</span>
                 </div>
-           </div>
-        );
-    },
-    render: function () {
-        var displayUpdate;
-//        if (this.props.userType === 'CHILD') {
-        displayUpdate = this.renderChild;
-//        } else {
-//            displayUpdate = this.renderAdult;
-//        }
-        return (
-           <Panel header={CHANGE} className={this.props.className + ' standard ' + IDENTIFIER}>
-               {displayUpdate()}
-           </Panel>
+
+                {/* Step 2 container */}
+                <div className="container step-2-container">
+                    <Button
+                        className={ClassNames(
+                            'option',
+                            {
+                                selected: self.state.selected && self.state.selected === self.state.option,
+                                'not-selected': self.state.selected &&
+                                                self.state.option &&
+                                                self.state.selected !== self.state.option
+                            }
+                        )}
+                        onClick={self.handleSelect.bind(self, self.state.option)}
+                    >
+                        {self.state.option}
+                    </Button>
+                    <Button
+                        className={ClassNames(
+                            'last',
+                            {
+                                selected: self.state.selected && self.state.selected === self.state.last,
+                                'not-selected': self.state.selected &&
+                                                self.state.last &&
+                                                self.state.selected !== self.state.last
+                            }
+                        )}
+                        onClick={self.handleSelect.bind(self, self.state.last)}
+                    >
+                        {self.state.last}
+                    </Button>
+                    {/* Step 2 icon */}
+                    <span className="step-2">2</span>
+                </div>
+
+                {/* Step 3 container */}
+                <div className="container step-3-container">
+                    <span className="step-3">{LABELS.SAVE}</span>
+                    <Button
+                        className="save-btn"
+                        onClick={this.setNewUsername.bind(this)}
+                    />
+                </div>
+
+            </div>
         );
     }
-});
+
+    renderConfirmation() {
+        return (
+            <div className={`confirmation-container ${this.state.page}`}>
+                <img className="header" src={`${GLOBALS.MEDIA_URL}8aa292e3231eb77be37da7ae0225799e.png`} />
+                <span className="prompt">{COPY.CONFIRM_NOTICE_1}</span>
+                <span className="prompt">{COPY.CONFIRM_NOTICE_2}</span>
+                <Button
+                    className="cancel-btn"
+                    onClick={this.setOriginal.bind(this)}
+                />
+                <Button
+                    className="confirm-btn"
+                    onClick={this.setChildUsername.bind(this)}
+                />
+            </div>
+        );
+    }
+
+    renderLoginNotice() {
+        return (
+            <div className={`login-notice-container ${this.state.page}`}>
+                <img className="header" src={`${GLOBALS.MEDIA_URL}b9567bafdf9c186ccac49a3c32c45ae8.png`} />
+                <span className="prompt">{COPY.LOGIN_NOTICE}</span>
+                <span className="username">{this.state.username}</span>
+            </div>
+        );
+    }
+
+    render() {
+        var page;
+
+        if (this.state.page === 'confirm') {
+            page = this.renderConfirmation;
+        } else if (this.state.page === 'login-notice') {
+            page = this.renderLoginNotice;
+        } else {
+            page = this.renderGenerator;
+        }
+
+        return (
+            page.call(this)
+        );
+    }
+}
 
 export default UpdateUsername;
+
 
