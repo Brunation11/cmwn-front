2dbd16e5bf46061edc23e4bc12158858dbc9c2e5
Max Rafferty <maxprafferty@gmail.com>
2016-11-04 15:35:38 -0400

    ignore profile for all

--- src/components/site_nav.js
+++ src/components/site_nav.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router';
 import _ from 'lodash';
+import Shortid from 'shortid';
 
 import ClassNames from 'classnames';
 import PublicRoutes from 'public_routes';
@@ -10,7 +11,7 @@ import Util from 'components/util';
 var addHardcodedEntries = function (menuItems) {
     menuItems.unshift({url: '/profile', label: 'Activities'});
     menuItems.push({url: '/resources', label: 'Resource Center'});
-//    menuItems.push({url: `/user/${this.props.currentUser.user_id}/feed`, label: 'Feed'});
+    menuItems.push({url: `/user/${this.props.currentUser.user_id}/feed`, label: 'Feed'});
     menuItems.push({url: '/profile/edit', label: 'Edit My Profile'});
     menuItems.push({url: '/logout', label: 'Logout'});
     return menuItems;
@@ -19,6 +20,10 @@ var addHardcodedEntries = function (menuItems) {
 const IGNORED_ROUTES_FOR_CHILDREN = [
     'Resource Center',
     'Friends and Network',
+    'Flags'
+];
+
+const IGNORED_ROUTES_FOR_EVERYONE = [
     'Profile'
 ];
 
@@ -81,14 +86,16 @@ var SiteNav = React.createClass({
     renderNavItems: function () {
         var currentUrl;
         var menuItems = buildMenuRoutes(this.props.data);
-        menuItems = addHardcodedEntries.call(this, menuItems);
         //manually hidden items for children
-
-        menuItems = _.filter(menuItems, item => this.props.currentUser.type !== 'CHILD' || (
-            this.props.currentUser.type === 'CHILD' &&
-            !~IGNORED_ROUTES_FOR_CHILDREN.indexOf(item.label))
-        );
         menuItems = addHardcodedEntries.call(this, menuItems);
+
+        menuItems = _.filter(menuItems, item =>
+            !~IGNORED_ROUTES_FOR_EVERYONE.indexOf(item.label) && (
+                this.props.currentUser.type !== 'CHILD' || (
+                    this.props.currentUser.type === 'CHILD' &&
+                    !~IGNORED_ROUTES_FOR_CHILDREN.indexOf(item.label))
+            )
+        );
 
         if (sessionStorage == null) {
             return null;
@@ -108,7 +115,7 @@ var SiteNav = React.createClass({
                 className={ClassNames({
                     'active-menu': sessionStorage.activeItem === item.label
                 })}
-                key={`(${item.label})-${item.url}`}
+                key={Shortid.generate()}
             >
                 <Link
                     to={item.url}
