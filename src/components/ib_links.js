40f751bc5e5e44675712394c57c358040eddf5a5
Max Rafferty <maxprafferty@gmail.com>
2016-11-10 12:40:23 -0500

    removed /f/ from media links

--- src/components/ib_links.js
+++ src/components/ib_links.js
@@ -2,9 +2,9 @@ import GLOBALS from 'components/globals';
 
 const LINKS = {
     FAQS: {
-        STUDENT: `${GLOBALS.MEDIA_URL}/f/ad969fcf71ecda4f1e5a72f05863bf37.pdf`,
-        TEACHER: `${GLOBALS.MEDIA_URL}/f/09258b65b267583dbd0eedc434d5b11f.pdf`,
-        ADMIN: `${GLOBALS.MEDIA_URL}/f/15d775ce44bc7dec5e8c74333ef7c93b.pdf`,
+        STUDENT: `${GLOBALS.MEDIA_URL}/ad969fcf71ecda4f1e5a72f05863bf37.pdf`,
+        TEACHER: `${GLOBALS.MEDIA_URL}/09258b65b267583dbd0eedc434d5b11f.pdf`,
+        ADMIN: `${GLOBALS.MEDIA_URL}/15d775ce44bc7dec5e8c74333ef7c93b.pdf`,
     },
 };
 
