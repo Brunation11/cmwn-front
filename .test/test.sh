fa7c0577b8de7523cc1e8f434263bca321a97c01
Max Rafferty <maxprafferty@gmail.com>
2016-11-15 15:19:03 -0500

    made testscript runnable on single files

--- .test/test.sh
+++ .test/test.sh
@@ -1,9 +1,35 @@
-rm -r coverage
-rm -r lib
-export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR;
+FILENAME="./src"
+while getopts f: opt
+do
+    case "$opt" in
+        f) 
+            echo 'got hurr'
+            FILENAME="$OPTARG";;
+        \?) #unknown flag
+            echo >&2 \
+            "usage: $0 [-f filename]"
+            exit 1;;
+    esac
+done
+PARTB=
+echo "*****************************************$FILENAME"
 export BABEL_ENV="production"
 export NODE_ENV="production"
-../node_modules/babel-cli/bin/babel.js ../src --retain-lines --out-dir lib --copy-files
-export NODE_PATH=${DIR}/lib
-../node_modules/istanbul/lib/cli.js cover --verbose --print both -x 'lib/mocks/**/*.js' -x 'lib/**/*.test.js' ../node_modules/mocha/bin/_mocha -- 'lib/**/*.test.js' --require ignore-styles --require ./lib/testdom.js
+export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR;
+rm -r coverage
+../node_modules/babel-cli/bin/babel.js ../$FILENAME ../src/testdom.js --retain-lines --out-dir src --copy-files
+if [ "$FILENAME" = "./src" ]
+    then
+        echo 'testing all files'
+    else
+        PARTB=$(find src -name \*.js | grep -v "$FILENAME" | sed -e 's/^/-x /')
+        echo "testing $FILENAME"
+fi
+PARTA="../node_modules/istanbul/lib/cli.js cover --verbose --print both -x 'src/mocks/**/*.js' -x 'src/**/*.test.js' "
+PARTC=" ../node_modules/mocha/bin/_mocha -- 'src/routes/users/profile.test.js' --require ignore-styles --require ./src/testdom.js"
+RUNTEST=$PARTA$PARTB$PARTC
+echo $RUNTEST
+export NODE_PATH=${DIR}/src
+eval $RUNTEST
+#../node_modules/istanbul/src/cli.js cover --verbose --print both -x 'src/mocks/**/*.js' -x 'src/**/*.test.js' ../node_modules/mocha/bin/_mocha -- 'src/**/*.test.js' --require ignore-styles --require ./src/testdom.js
