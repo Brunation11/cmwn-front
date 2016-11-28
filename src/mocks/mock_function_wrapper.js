31d69eae821527200e7a80d7cbdebf5aad9f70a4
Annalise Mariottini <annaliseirene@msn.com>
2016-11-28 18:53:16 -0500

    new mocks, added functionality to mock function wrapper

--- src/mocks/mock_function_wrapper.js
+++ src/mocks/mock_function_wrapper.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import  _ from 'lodash';
 
 class MockFunctionWrapper extends React.Component {
     constructor() {
         super();
+        this.state = {};
     }
 
-    render() {
-        return this.props.render.call(this, this.props.item);
+    componentWillMount() {
+        this.render = this.props.render.bind(this, this.props.item);
+
+        if (this.props.functions) {
+            _.forEach(this.props.functions, (func, funcName) => {
+                this[funcName] = func.bind(this);
+            });
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (!nextProps.state) return;
+
+        this.setState({ [nextProps.state.key]: [nextProps.state.value] }, () => {
+            console.log('************\n' + this.state);
+        });
     }
 }
 
