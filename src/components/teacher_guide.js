8735a36f08bdda987d837583208d1d7afd559497
Max Rafferty <maxprafferty@gmail.com>
2016-11-28 19:16:28 -0500

    rough modal styles

--- src/components/teacher_guide.js
+++ src/components/teacher_guide.js
@@ -1,12 +1,12 @@
 
 import React from 'react';
-import {Panel, ButtonGroup, Button, DropdownButton, MenuItem} from 'react-bootstrap';
-import _ from 'lodash';
-import Shortid from 'shortid';
+import {Modal, Button} from 'react-bootstrap';
+//import _ from 'lodash';
+//import Shortid from 'shortid';
 //import ClassNames from 'classnames';
 
-import GLOBALS from 'components/globals';
-import ACTION_CONSTANTS from 'components/action_constants';
+//import GLOBALS from 'components/globals';
+//import ACTION_CONSTANTS from 'components/action_constants';
 
 import 'components/teacher_guide.scss';
 
@@ -22,35 +22,58 @@ const BUTTON_TEXT = 'Teacher\'s Guide';
 class Guide extends React.Component {
     constructor() {
         super();
-        this.state = {};
+        this.state = {
+            modalOpen: false,
+            page: 1
+        };
     }
 
     componentDidMount() {
-        this.setupState(this.props);
     }
 
-    componentWillReceiveProps(nextProps) {
-        this.setupState(nextProps);
+    openModal() {
+        this.setState({modalOpen: true});
     }
 
-    setupState(props) {
-        var state = {};
-        state.base = props.base;
-        state.uuid = props.uuid || props.id;
-        // state.canUpdate = props.canUpdate !=
-        //     null ? props.canUpdate : Util.decodePermissions(props.scope).update;
-        this.setState(state);
+    closeModal() {
+        this.setState({modalOpen: false});
+    }
+
+    renderModal() {
+        return (
+            <div className="modal-guide">
+                BORF
+                <a id="close-modal" onClick={this.closeModal.bind(this)} className="modal-close">
+                    <span className="close-x">X</span>
+                </a>
+            </div>
+        );
     }
 
     render() {
-        if (!this.props.isAdult) {
+        var self = this;
+        if (!self.props.isAdult) {
             return null;
         }
         return (
-        <div className={COMPONENT_IDENTIFIER}>{CALL_TO_ACTION}<Button className="standard purple">{BUTTON_TEXT}</Button></div>
+        <div className={COMPONENT_IDENTIFIER}>{CALL_TO_ACTION}
+            <Modal
+                className="guide-modal"
+                show={this.state.modalOpen}
+                onHide={self.closeModal.bind(self)}
+                keyboard={false}
+                backdrop="static"
+                id="guide-modal"
+            >
+                <Modal.Body>
+                    {self.renderModal()}
+                </Modal.Body>
+            </Modal>
+            <Button className="standard purple" onClick={self.openModal.bind(self)}>{BUTTON_TEXT}</Button>
+        </div>
         );
     }
-};
+}
 
 Guide.defaultProps = {isAdult: true};
 
