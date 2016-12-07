e79c6cc530ed497f8e5eb66bffd3943e510f1c81
Max Rafferty <maxprafferty@gmail.com>
2016-12-07 17:27:11 -0500

    update es5 sham to latest, CORE-2471

--- src/index.php
+++ src/index.php
@@ -131,7 +131,7 @@ if (!$https && $proxy !== 'https') {
             <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
             <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv-printshiv.min.js"></script>
             <script src="//cdnjs.cloudflare.com/ajax/libs/es5-shim/3.4.0/es5-shim.js"></script>
-            <script src="//cdnjs.cloudflare.com/ajax/libs/es5-shim/3.4.0/es5-sham.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.5.9/es5-sham.min.js" integrity="sha256-VWp+JQdfSrhpjCFLnytj5imNZ6utXYSk2vVGgsekbok=" crossorigin="anonymous"></script>
         <![endif]-->
         <!--[if IE]>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/flexie/1.0.3/flexie.min.js"></script>
