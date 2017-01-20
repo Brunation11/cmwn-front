02a8caa75a7072b4035e74278e2b1e606334ab12
Max Rafferty <maxprafferty@gmail.com>
2017-01-20 16:35:48 -0500

    fixed fallback

--- src/components/game.js
+++ src/components/game.js
@@ -180,10 +180,14 @@ export class Game extends React.Component {
 
     exitFullscreen() {
         Screenfull.exit();
+        window.document.body.className =
+            _.without(window.document.body.className.split(' '), 'fullscreen').join(' ');
         this.setState({
             fullscreenFallback: false,
             isFullscreen: false,
-        });
+        }, this.resizeFrame);
+
+        this.resizeFrame();
     }
 
     resizeFrame() {
@@ -207,13 +211,14 @@ export class Game extends React.Component {
     makeFullScreen() {
         var nextState = {isFullscreen: true};
         window.document.body.className = window.document.body.className + ' fullscreen';
-        if (false && Screenfull.enabled) {
+        if (Screenfull.enabled) {
             Screenfull.request(document.body);
         } else {
             nextState.fullscreenFallback = true;
             this.resizeFrame();
         }
-        this.setState(nextState, () => { this.resizeFrame(); });
+        this.setState(nextState, this.resizeFrame );
+
     }
 
     checkForPortrait() {
