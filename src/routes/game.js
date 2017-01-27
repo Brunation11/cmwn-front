a9102f80021f77db5761261f445df7283029cab3
Max Rafferty <maxprafferty@gmail.com>
2017-01-27 18:00:41 -0500

    fixes for unit test failures

--- src/routes/game.js
+++ src/routes/game.js
@@ -22,7 +22,9 @@ export class GamePage extends React.Component {
     }
     componentDidMount() {
         var gameElem = ReactDOM.findDOMNode(this.refs.gameRef);
-        gameElem.scrollIntoView();
+        if (gameElem && gameElem.scrollIntoView) { //defensive code only needed for error in testDOM
+            gameElem.scrollIntoView();
+        }
 
         this.resolveRole(this.props);
         this.setState({
