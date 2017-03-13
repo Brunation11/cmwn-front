6e8759e2adddb9ff8c372ea8d19b99e5df47cf1a
bunation11 <bzatta@gmail.com>
2017-03-12 17:57:40 -0700

    add check to links to ensure props

--- src/components/game_events.js
+++ src/components/game_events.js
@@ -206,7 +206,7 @@ export default function (eventPrefix, gameId, _links, exitCallback) {
                 .catch(err => Log.error(err));
         },
         getFriends: function (e) {
-            if (_links.friend) {
+            if (_.links && _links.friend) {
                 HttpManager.GET(_links.friend.href)
                     .then(server => {
                         var friends = _.map(server.response._embedded.friend, friend => {
