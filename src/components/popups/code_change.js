8b578935b37bb8e9db101b14f2a03440eeea7c4c
bunation11 <bzatta@gmail.com>
2017-03-26 18:06:10 -0700

    update code change popup component, update styles

--- src/components/popups/code_change.js
+++ src/components/popups/code_change.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import {Modal} from 'react-bootstrap';
 
-// import 'components/popups/code_change.scss';
+import 'components/popups/code_change.scss';
 
 const HEADER = 'BE SURE!';
-const COPY = 'To share this code with the user. It will only be active for 24 hours!';
+const COPY = (
+    <span>
+        To share this code with the user.
+        <br />
+        It will only be active for <span className="callout">24 hours!</span>
+    </span>
+);
 
 export default class CodeChangePopup extends React.Component {
     constructor() {
         super();
 
         this.state = {
-            open: true
+            open: false
         };
     }
 
@@ -35,9 +41,11 @@ export default class CodeChangePopup extends React.Component {
             >
                 <Modal.Body>
                     <div>
-                        <h1>{HEADER}</h1>
-                        <span>{COPY}</span>
-                        <button className="close-modal" onClick={this.hideModal}>OK</button>
+                        <h1>{this.props.header || HEADER}</h1>
+                        <div className="copy">
+                            {this.props.copy || COPY}
+                        </div>
+                        <button className="close-modal" onClick={this.hideModal.bind(this)}>OK</button>
                     </div>
                 </Modal.Body>
             </Modal>
